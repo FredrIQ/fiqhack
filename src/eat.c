1ab753622322b8a108cb5bd78d17aee334237ea3
Patric Müller <bhaak@gmx.net>
2011-11-23 19:04:11 +0100

    fix C343-116 Grammar error in messages related to choking on gold.

--- src/eat.c
+++ src/eat.c
@@ -218,13 +218,13 @@ static void choke(struct obj *food) /* To a full belly all food is bad. (It.) */
 	} else {
 		killer_format = KILLED_BY_AN;
 		/*
-		 * Note all "killer"s below read "Choked on %s" on the
+		 * Note all "killer"s below read "Choked on a/an %s" on the
 		 * high score list & tombstone.  So plan accordingly.
 		 */
 		if (food) {
 			pline("You choke over your %s.", foodword(food));
 			if (food->oclass == COIN_CLASS) {
-				killer = "a very rich meal";
+				killer = "very rich meal";
 			} else {
 				killer = food_xname(food, FALSE);
 				if (food->otyp == CORPSE &&
