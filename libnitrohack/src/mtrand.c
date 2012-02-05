3ff432aa89316da6ed1a391db8d4eb7887b39b94
Daniel Thaler <daniel@dthaler.de>
2012-02-05 01:44:52 +0100

    display rng paranoia
    The display rng re-uses state from the main sequence without advancing
    it. That is good enough for display purposes, but has the potential to
    leak info about the main rng sequence (especially during hallucination,
    which uses a large number of random values).
    Now the main rng state is only mixed into the output of an lcg prng,
    which should make deducing any information impossible.

--- libnitrohack/src/mtrand.c
+++ libnitrohack/src/mtrand.c
@@ -131,8 +131,17 @@ void restore_mt_state(struct memfile *mf)
  * seems like overkill. */
 int display_rng(int x)
 {
+    static unsigned lcg_val = 1073741827;
     unsigned int num = state[dpos];
     dpos = (dpos + 1) % N;
-    return num % x;
+    /* a simple lcg generator which modifies the outpur from the mersenn sequence
+     * this is pure paranoia, induced by ais523, in order to avoid leaking info
+     * about the main rng sequence during hallucination
+     * 4294967291 is the largest prime under 2^32; 65537 and 7 are also prime.
+     * This gives the lcg a full period according to Hull & Dobell.
+     */
+    lcg_val = ((lcg_val * 7 + 65537) % 4294967291) ^ num;
+    
+    return lcg_val % x;
 }
 
