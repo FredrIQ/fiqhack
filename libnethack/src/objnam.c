7f40a1e34e77f8411267877a3ebf43be81456055
Sean <scshunt@csclub.uwaterloo.ca>
2012-04-29 00:07:30 -0400

    Fix minor memory errors---and pick up an obscure pluralization bug too.

--- libnethack/src/objnam.c
+++ libnethack/src/objnam.c
@@ -1388,12 +1388,12 @@ char *makeplural(const char *oldstr)
 			!strcmp(spot-4, "ninja") ||
 			!strcmp(spot-4, "ronin") ||
 			!strcmp(spot-4, "shito") ||
-			!strcmp(spot-7, "shuriken") ||
 			!strcmp(spot-4, "tengu") ||
 			!strcmp(spot-4, "manes"))) ||
 	    (len >= 6 && !strcmp(spot-5, "ki-rin")) ||
 	    (len >= 6 && !strcmp(spot-5, "Nazgul")) ||
-	    (len >= 7 && !strcmp(spot-6, "gunyoki")))
+	    (len >= 7 && (!strcmp(spot-6, "gunyoki") ||
+			!strcmp(spot-7, "shuriken"))))
 		goto bottom;
 
 	/* man/men ("Wiped out all cavemen.") */
@@ -1497,7 +1497,7 @@ char *makeplural(const char *oldstr)
 		goto bottom;
 	}
 	if (len >= 5 && (!strcmp(spot-4, "matzo")
-					|| !strcmp(spot-5, "matza"))) {
+					|| !strcmp(spot-4, "matza"))) {
 		strcpy(spot, "ot");
 		goto bottom;
 	}
