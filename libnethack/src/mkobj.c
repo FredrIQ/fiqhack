1aefd90e010caa6fdafad9659bae97ff59fd1dc7
Sean <scshunt@csclub.uwaterloo.ca>
2012-04-29 01:14:36 -0400

    Fix a bug where bones with corpses would corrupt memory.

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -873,18 +873,16 @@ struct obj *mkcorpstat(int objtype,	/* CORPSE or STATUE */
 struct obj *obj_attach_mid(struct obj *obj, unsigned mid)
 {
     struct obj *otmp;
-     int lth, namelth;
+    int namelth;
+
+    if (obj->oxlth) {
+        impossible("xlth set when attaching mid");
+        return otmp;
+    }
 
     if (!mid || !obj) return NULL;
-    lth = sizeof(mid);
     namelth = obj->onamelth ? strlen(ONAME(obj)) + 1 : 0;
-    if (namelth) 
-	otmp = realloc_obj(obj, lth, &mid, namelth, ONAME(obj));
-    else {
-	otmp = obj;
-	otmp->oxlth = sizeof(mid);
-	memcpy(otmp->oextra, (void *)&mid, sizeof(mid));
-    }
+    otmp = realloc_obj(obj, sizeof mid, &mid, namelth, ONAME(obj));
     if (otmp && otmp->oxlth) otmp->oattached = OATTACHED_M_ID;	/* mark it */
     return otmp;
 }
@@ -1364,7 +1362,7 @@ void set_obj_level(struct level *lev, struct obj *obj)
 struct obj *restore_obj(struct memfile *mf)
 {
     unsigned int oflags;
-    char namelen;
+    char namelen, oattached;
     struct obj *otmp;
     
     mfmagic_check(mf, OBJ_MAGIC);
