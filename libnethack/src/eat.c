e0b3f33f53d0924c8e9293680c15e7645b3b9188
Steve Melenchuk <smelenchuk@gmail.com>
2012-05-03 13:53:43 -0600

    Work a call to touch_artifact into the eating non-food code, such that a player can't eat an artifact they can't touch.  Fixes C343-244. (cherry picked from commit 7ed17843988954ae3235b3d5ef7ff30c3f0ba183)
    Conflicts:
    
    	libnethack/src/eat.c

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1763,6 +1763,13 @@ int doeat(struct obj *otmp)	/* generic "eat" command funtion (see cmd.c) */
 	}
 	if (otmp->oclass != FOOD_CLASS) {
 	    int material;
+
+	    if (!touch_artifact(otmp,&youmonst)) return 1;
+	    if (!is_edible(otmp)) {
+	        pline("You can no longer eat %s.", doname(otmp));
+		return 1;
+	    }
+
 	    victual.reqtime = 1;
 	    victual.piece = otmp;
 		/* Don't split it, we don't need to if it's 1 move */
