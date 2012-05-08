cb6a6c911c4b869b38668db5e54ac4cae051b783
Jared Minch <jmminch@gmail.com>
2012-05-08 15:39:27 -0700

    Adjust weight calculation for bags of holding to round up rather than simply adding 1.
     Prevents bag of holding from weighing more than other sacks when empty.
     Possibly fixes C343-418.  Makes identifying bags of holding slightly less
     trivial.

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -750,8 +750,8 @@ int weight(struct obj *obj)
 		 *	Bag status	Weight of contents
 		 *	----------	------------------
 		 *	cursed			2x
-		 *	blessed			x/4 + 1
-		 *	otherwise		x/2 + 1
+		 *	blessed			x/4 (rounded up)
+		 *	otherwise		x/2 (rounded up)
 		 *
 		 *  The macro DELTA_CWT in pickup.c also implements these
 		 *  weight equations.
@@ -763,8 +763,8 @@ int weight(struct obj *obj)
 		 */
 		if (obj->otyp == BAG_OF_HOLDING)
 		    cwt = obj->cursed ? (cwt * 2) :
-					(1 + (cwt / (obj->blessed ? 4 : 2)));
-
+                                        (obj->blessed ? (cwt + 3) / 4 :
+                                                        (cwt + 1) / 2);
 		return wt + cwt;
 	}
 	if (obj->otyp == CORPSE && obj->corpsenm >= LOW_PM) {
