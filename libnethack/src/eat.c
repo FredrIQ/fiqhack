sgrunt/GruntHack

Rework rndmonnam() into three functions, one returning a random monster index, one returning the name associated with that index, and one returning if the name associated with the index is a proper name; this allows the correct prefix (or lack thereof) to be used with a random monster name.  Fixes C343-155.

src/eat.c
@@ -1099,7 +1099,13 @@ opentin(VOID_ARGS)		/* called during each move whilst opening a tin */
 		r = ROTTEN_TIN;			/* some homemade tins go bad */
 	    which = 0;	/* 0=>plural, 1=>as-is, 2=>"the" prefix */
 	    if (Hallucination) {
-		what = rndmonnam();
+	    	int name = rndmonidx();
+		what = monnam_for_index(name);
+		if ((name < SPECIAL_PM) &&
+		    !!(mons[tin.tin->corpsenm].geno & G_UNIQ))
+		    which = type_is_pname(&mons[name]) ? 1 : 2;
+		else
+		    which = monnam_is_pname(name) ? 1 : 0;
 	    } else {
 		what = mons[tin.tin->corpsenm].mname;
 		if (mons[tin.tin->corpsenm].geno & G_UNIQ)

src/priest.c
@@ -248,9 +248,18 @@ priestname(mon, pname)
 register struct monst *mon;
 char *pname;		/* caller-supplied output buffer */
 {
-	const char *what = Hallucination ? rndmonnam() : mon->data->mname;
+	const char *what;
+	boolean do_the = TRUE;
+	if (Hallucination) {
+	    int name = rndmonidx();
+	    what = monnam_for_index(name);
+	    do_the = !monnam_is_pname(name);
+	} else {
+	    what = mon->data->mname;
+	}
 
-	Strcpy(pname, "the ");
+	if (do_the)
+		Strcpy(pname, "the ");
 	if (mon->minvis) Strcat(pname, "invisible ");
 	if (mon->ispriest || roamer_type(mon->data)) {
 		/* use epri */

FredrIQ/fiqhack

Rework rndmonnam() into three functions, one returning a random monster index, one returning the name associated with that index, and one returning if the name associated with the index is a proper name; this allows the correct prefix (or lack thereof) to be used with a random monster name.  Fixes C343-155. (cherry picked from commit f809ede38f087149d9276a4e46ba91fbeca6a216)
Had to make a bunch of changes on this one. Some grammar problems.
Conflicts:
	include/extern.h
	libnethack/src/potion.c
	libnethack/src/priest.c
	libnethack/src/timeout.c
	src/do_name.c
	src/muse.c
	src/pickup.c

libnethack/src/eat.c
@@ -886,7 +886,13 @@ static int opentin(void) /* called during each move whilst opening a tin */
 		r = ROTTEN_TIN;			/* some homemade tins go bad */
 	    which = 0;	/* 0=>plural, 1=>as-is, 2=>"the" prefix */
 	    if (Hallucination) {
-		what = rndmonnam();
+	    	int idx = rndmonidx();
+		what = monnam_for_index(idx);
+		if ((idx < SPECIAL_PM) &&
+		    !!(mons[tin.tin->corpsenm].geno & G_UNIQ))
+		    which = type_is_pname(&mons[idx]) ? 1 : 2;
+		else
+		    which = monnam_is_pname(idx) ? 1 : 0;
 	    } else {
 		what = mons[tin.tin->corpsenm].mname;
 		if (mons[tin.tin->corpsenm].geno & G_UNIQ)

libnethack/src/priest.c
@@ -221,9 +221,18 @@ void priestini(struct level *lev, struct mkroom *sroom, int sx, int sy,
 char *priestname(const struct monst *mon,
 		 char *pname)	/* caller-supplied output buffer */
 {
-	const char *what = Hallucination ? rndmonnam() : mon->data->mname;
+	const char *what;
+	boolean do_the = TRUE;
+	if (Hallucination) {
+	    int idx = rndmonidx();
+	    what = monnam_for_index(idx);
+	    do_the = !monnam_is_pname(idx);
+	} else {
+	    what = mon->data->mname;
+	}
 
-	strcpy(pname, "the ");
+	if (do_the)
+          strcpy(pname, "the ");
 	if (mon->minvis) strcat(pname, "invisible ");
 	if (mon->ispriest || mon->data == &mons[PM_ALIGNED_PRIEST] ||
 					mon->data == &mons[PM_ANGEL]) {