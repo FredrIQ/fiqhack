416dd3d741fb466e5609b9df557170d731dd1237
Tung Nguyen <tungtn3@gmail.com>
2012-10-19 21:28:32 +1100

    Favor room spaces during autoexplore
    This change causes autoexplore to favor room spaces over, say, corridors
    and other places to a factor of 5 to 1.  This should somewhat reduce the
    frequency of autoexplore zipping right through dark rooms.
    
    This is meant to go with commit a06d1c831d (autoexplore: prefer
    exploring items).
    
    (cherry picked from commit 500b497342aff97ea155f721b9fe00baa4f4e8de)

--- libnitrohack/src/hack.c
+++ libnitrohack/src/hack.c
@@ -768,10 +768,16 @@ unexplored(int x, int y)
  * The lower the value the better.*/
 static int autotravel_weighting(int x, int y, unsigned distance)
 {
-	if (level->locations[x][y].mem_obj) {
+	const struct rm *loc = &level->locations[x][y];
+
 	    /* greedy for items */
+	if (loc->mem_obj)
 	    return distance;
-	}
+
+	/* favor rooms */
+	if (loc->roomno)
+	    return distance * 2;
+
 	/* by default return distance multiplied by a large constant factor */
 	return distance * 10;
 }
