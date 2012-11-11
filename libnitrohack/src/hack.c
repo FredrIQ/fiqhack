8a938b60bcba6599dbd0286b4f08550eb20579a2
Tung Nguyen <tungtn3@gmail.com>
2012-11-11 13:56:54 +1100

    Autoexplore: Disable when weak from hunger
    Instead of exploring, autoexplore will give the message, "You feel too
    weak from hunger to explore" when weak from hunger.  This should stop
    repeated presses of the 'v' key bringing the character into the fainting
    state without realizing it.
    
    I was going to put this at the fainting level, but fainting from hunger
    already stops autoexplore and usually occurs within 20 turns of entering
    the state anyway.
    
    (cherry picked from commit 36f97a7faefbcb30e6002a162b406833694f443f)

--- libnitrohack/src/hack.c
+++ libnitrohack/src/hack.c
@@ -1046,6 +1046,11 @@ domove(schar dx, schar dy, schar dz)
                 nomul(0, NULL);
                 return 0;
             }
+		if (u.uhs >= WEAK) {
+		    pline("You feel too weak from hunger to explore.");
+		    nomul(0, NULL);
+		    return 0;
+		}
             u.tx = u.ux;
             u.ty = u.uy;
             if (!findtravelpath(unexplored, &dx, &dy)) {
