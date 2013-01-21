vinceho/nethack4

Better handle moats with new is_moat function.
Also fixup a trailing space and make more use if is_* for other types of
terrain.

libnethack/src/dbridge.c
@@ -33,10 +33,7 @@ is_pool(struct level *lev, int x, int y)
     if (!isok(x, y))
         return FALSE;
     ltyp = lev->locations[x][y].typ;
-    if (ltyp == POOL || ltyp == MOAT || ltyp == WATER)
-        return TRUE;
-    if (ltyp == DRAWBRIDGE_UP &&
-        (lev->locations[x][y].drawbridgemask & DB_UNDER) == DB_MOAT)
+    if (ltyp == POOL || ltyp == WATER || is_moat(lev, x, y))
         return TRUE;
     return FALSE;
 }
@@ -71,6 +68,20 @@ is_ice(struct level * lev, int x, int y)
     return FALSE;
 }
 
+boolean
+is_moat(struct level * lev, int x, int y)
+{
+    schar ltyp;
+
+    if (!isok(x, y))
+        return FALSE;
+    ltyp = lev->locations[x][y].typ;
+    if (!Is_juiblex_level(lev) &&
+        (ltyp == MOAT || (ltyp == DRAWBRIDGE &&
+         (lev->locations[x][y].drawbridgemask & DB_UNDER) == DB_MOAT)))
+        return TRUE;
+    return FALSE;
+}
 
 /*
  * We want to know whether a wall (or a door) is the portcullis (passageway)

libnethack/src/dig.c
@@ -470,17 +470,13 @@ fillholetyp(int x, int y)
 
     for (x1 = lo_x; x1 <= hi_x; x1++)
         for (y1 = lo_y; y1 <= hi_y; y1++)
-            if (level->locations[x1][y1].typ == POOL)
-                pool_cnt++;
-            else if (level->locations[x1][y1].typ == MOAT ||
-                     (level->locations[x1][y1].typ == DRAWBRIDGE_UP &&
-                      (level->locations[x1][y1].drawbridgemask & DB_UNDER) ==
-                      DB_MOAT))
+            if (is_moat(lev, x1, y1))
                 moat_cnt++;
-            else if (level->locations[x1][y1].typ == LAVAPOOL ||
-                     (level->locations[x1][y1].typ == DRAWBRIDGE_UP &&
-                      (level->locations[x1][y1].drawbridgemask & DB_UNDER) ==
-                      DB_LAVA))
+            else if (is_pool(lev, x1, y1))
+                /* This must come after is_moat since moats are pools but
+                 * not vice-versa. */
+                pool++;
+            else if (is_lava(lev, x1, y1))
                 lava_cnt++;
     pool_cnt /= 3;      /* not as much liquid as the others */
 
@@ -686,7 +682,7 @@ dighole(boolean pit_only)
 
     } else if (loc->typ == DRAWBRIDGE_DOWN ||
                (is_drawbridge_wall(u.ux, u.uy) >= 0)) {
-        /* drawbridge_down is the platform crossing the moat when the bridge is 
+        /* drawbridge_down is the platform crossing the moat when the bridge is
            extended; drawbridge_wall is the open "doorway" or closed "door"
            where the portcullis/mechanism is located */
         if (pit_only) {

FredrIQ/fiqhack

libnethack/src/dbridge.c
@@ -34,10 +34,7 @@ is_pool(struct level *lev, int x, int y)
     if (!isok(x, y))
         return FALSE;
     ltyp = lev->locations[x][y].typ;
-    if (ltyp == POOL || ltyp == MOAT || ltyp == WATER)
-        return TRUE;
-    if (ltyp == DRAWBRIDGE_UP &&
-        (lev->locations[x][y].drawbridgemask & DB_UNDER) == DB_MOAT)
+    if (ltyp == POOL || ltyp == WATER || is_moat(lev, x, y))
         return TRUE;
     return FALSE;
 }
@@ -72,6 +69,20 @@ is_ice(struct level * lev, int x, int y)
     return FALSE;
 }
 
+boolean
+is_moat(struct level * lev, int x, int y)
+{
+    schar ltyp;
+
+    if (!isok(x, y))
+        return FALSE;
+    ltyp = lev->locations[x][y].typ;
+    if (!Is_juiblex_level(&lev->z) &&
+        (ltyp == MOAT || (ltyp == DRAWBRIDGE_UP &&
+         (lev->locations[x][y].drawbridgemask & DB_UNDER) == DB_MOAT)))
+        return TRUE;
+    return FALSE;
+}
 
 /*
  * We want to know whether a wall (or a door) is the portcullis (passageway)

libnethack/src/dig.c
@@ -470,17 +470,13 @@ fillholetyp(int x, int y)
 
     for (x1 = lo_x; x1 <= hi_x; x1++)
         for (y1 = lo_y; y1 <= hi_y; y1++)
-            if (level->locations[x1][y1].typ == POOL)
-                pool_cnt++;
-            else if (level->locations[x1][y1].typ == MOAT ||
-                     (level->locations[x1][y1].typ == DRAWBRIDGE_UP &&
-                      (level->locations[x1][y1].drawbridgemask & DB_UNDER) ==
-                      DB_MOAT))
+            if (is_moat(level, x1, y1))
                 moat_cnt++;
-            else if (level->locations[x1][y1].typ == LAVAPOOL ||
-                     (level->locations[x1][y1].typ == DRAWBRIDGE_UP &&
-                      (level->locations[x1][y1].drawbridgemask & DB_UNDER) ==
-                      DB_LAVA))
+            else if (is_pool(level, x1, y1))
+                /* This must come after is_moat since moats are pools but
+                 * not vice-versa. */
+                pool_cnt++;
+            else if (is_lava(level, x1, y1))
                 lava_cnt++;
     pool_cnt /= 3;      /* not as much liquid as the others */
 
@@ -686,7 +682,7 @@ dighole(boolean pit_only)
 
     } else if (loc->typ == DRAWBRIDGE_DOWN ||
                (is_drawbridge_wall(u.ux, u.uy) >= 0)) {
-        /* drawbridge_down is the platform crossing the moat when the bridge is 
+        /* drawbridge_down is the platform crossing the moat when the bridge is
            extended; drawbridge_wall is the open "doorway" or closed "door"
            where the portcullis/mechanism is located */
         if (pit_only) {