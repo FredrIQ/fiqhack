ab1973a54740da9f5d4e89a17dbc75a45dbcea5d
Tung Nguyen <tungtn3@gmail.com>
2013-07-16 21:18:17 +1000

    Don't echo bogus coordinates when cancelling a position prompt
    Pressing escape when choosing a location/position now shows "<position:
    cancelled>" instead of "<position: (xx, yy)>" where xx and yy didn't
    correspond to anything meaningful.
    
    (cherry picked from commit 7ea9a49b3480d95104100093705ce88e4e44ea38)
    
    Conflicts:
    	libnitrohack/src/windows.c

--- libnitrohack/src/windows.c
+++ libnitrohack/src/windows.c
@@ -20,8 +20,11 @@ getpos(coord * cc, boolean force, const char *goal)
         rv = (*windowprocs.win_getpos) (&x, &y, force, goal);
     } while (force && (rv == -1 || x < 1 || y < 1 || x > COLNO || y > ROWNO));
     log_getpos(rv, x, y);
+    if (rv == -1)
+        pline("<position: (cancelled)>");
+    else
+        pline("<position: (%d, %d)>", cc->x, cc->y);
     suppress_more();
-    pline("<position: (%d, %d)>", cc->x, cc->y);
 
     cc->x = x;
     cc->y = y;
