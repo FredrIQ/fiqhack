cf1cc1d2cc729e5b0fe3a42c21a46845ef66a331
Alex Smith <ais523@nethack4.org>
2013-09-21 14:26:34 +0100

    Timestamps for license compliance
    So far, we've only been distributing via VCS, which mixes the
    authorship information for the files into the same packs that
    everything else is distributed with. However, some day we might
    distribute the files individually, in which case (according to
    the NHGPL) they each need their own, individual, change details.
    
    This change adds a last-modified author and date to each file in
    the distribution, as well as a script that allows them to be
    easily updated. (The intended workflow is to run the script and
    commit its results just before each push.)
    
    All the last-modified author/dates are me and today, because I
    just changed the files to put the last-modified dates on them...

--- libnethack/include/align.h
+++ libnethack/include/align.h
@@ -1,4 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
+/* Last modified by Alex Smith, 2013-09-21 */
 /* Copyright (c) Mike Stephenson, Izchak Miller  1991.            */
 /* NetHack may be freely redistributed.  See license for details. */
 
