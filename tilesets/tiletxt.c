f22be5bffe9cf3608ddb1751e71c98585a707e49
Alex Smith <ais523@nethack4.org>
2013-09-22 22:23:13 +0100

    Work out what tile utilities we want, rename files to match

--- tilesets/tiletxt.c
+++
@@ -1,6 +0,0 @@
-/* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-09-21 */
-/* Copyright (C) Alex Smith, 2013 */
-/* NetHack may be freely redistributed.  See license for details. */
-#define TILETEXT
-#include "tilemap.c"
