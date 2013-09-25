a3a9f6caf480a5b73100b61ab8869550e17efdd7
Alex Smith <ais523@nethack4.org>
2013-09-25 13:03:57 +0100

    Use author not commiter timestamps in update-timestamps.sh
    It's unclear what the letter of the license means with respect to a
    VCS, anyway (which file, specifically, is being changed?); and using
    the author timestamp seems to fit the spirit of the license better
    (i.e. the time at which the file itself was changed, as opposed to
    the time at which the change was added to the repository).

--- update-timestamps.sh
+++ update-timestamps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Last modified by Alex Smith, 2013-09-21
+# Last modified by Alex Smith, 2013-09-25
 for x in `git ls-tree -r HEAD | cut -f 2`
 do
-    sed -i -e "1,2s/Last modified by.*, ....-..-../$(git log -n 1 --pretty=format:'Last modified by %cn, %ct' $x | perl -pe 's/(\d+)$/`date --date=\@$1 +%F`/e')/" $x
+    sed -i -e "1,2s/Last modified by.*, ....-..-../$(git log -n 1 --pretty=format:'Last modified by %an, %at' $x | perl -pe 's/(\d+)$/`date --date=\@$1 +%F`/e')/" $x
 done
