063cd1170f0ea72781cf9c963c1e1070eebfcd66
Alex Smith <ais523@nethack4.org>
2013-10-04 00:30:11 +0100

    Disable non-working font sizes and tile sizes
    Switching fonts isn't currently implemented (and the obvious
    implementation drives SDL crazy, so there's probably something wrong
    in sdl.c that should be fixed later). 3D tiles aren't really
    implemented at all (the tiles utilites understand them, but the
    windowports don't); and both the 3D and 32x32 tiles are missing
    images. (A necessary step before they can be used is to convert the
    16x16 images to do transparency properly.)
    
    Thus, for now, we don't install the non-working fonts/tilesets, and
    don't give the user the option to select them.

--- nethack/src/options.c
+++ nethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-10-03 */
+/* Last modified by Alex Smith, 2013-10-04 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -57,8 +57,8 @@ static struct nh_listitem graphics_list[] = {
     {ASCII_GRAPHICS, "Text (ASCII only)"},
     {UNICODE_GRAPHICS, "Text (Unicode)"},
     {TILESET_16, "Tiles (16x16)"},
-    {TILESET_32, "Tiles (32x32)"},
-    {TILESET_3D, "Tiles (3D effect)"},
+/*    {TILESET_32, "Tiles (32x32)"},
+      {TILESET_3D, "Tiles (3D effect)"}, */
 };
 static struct nh_enum_option graphics_spec =
     { graphics_list, listlen(graphics_list) };
