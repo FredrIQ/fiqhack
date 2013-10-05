8d63b9d8ef676b23ce4053f3b0ff0fef81d7486f
Alex Smith <ais523@nethack4.org>
2013-10-05 10:50:26 +0100

    Last-modified dates

--- libnethack_client/src/netcmd.c
+++ libnethack_client/src/netcmd.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-09-21 */
+/* Last modified by Alex Smith, 2013-10-05 */
 /* Copyright (c) Daniel Thaler, 2012. */
 /* The NetHack client lib may be freely redistributed under the terms of either:
  *  - the NetHack license
