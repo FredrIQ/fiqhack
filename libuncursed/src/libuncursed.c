63be0c3f69c1ef7c82bc88333725ca874077fe67
Alex Smith <ais523@nethack4.org>
2013-10-16 20:19:19 +0100

    Timestamp updates for timestamp updates
    This is ridiculous :)

--- libuncursed/src/libuncursed.c
+++ libuncursed/src/libuncursed.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2013-10-11 */
+/* Last modified by Alex Smith, 2013-10-16 */
 /* Copyright (c) 2013 Alex Smith. */
 /* The 'uncursed' rendering library may be distributed under either of the
  * following licenses:
