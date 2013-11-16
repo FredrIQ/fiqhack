bfade3b591f6e398b885d70ba4334fdf575e234e
Tung Nguyen <tungtn3@gmail.com>
2013-11-16 18:42:42 +1100

    Enable hit bonuses for martial arts/bare-handed combat
    NetHack has always had code to calculate the to-hit bonus for unarmed
    combat, but never ran it due to faulty logic, which this commit fixes.
    
    From SporkHack r140 and r188.
    
    (manually cherry-picked from 757c4d688151bb8819024a574f621a18d6c08db3)

--- libnethack/src/weapon.c
+++ libnethack/src/weapon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-10-19 */
+/* Last modified by Alex Smith, 2013-11-25 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -106,8 +106,13 @@ hitval(struct obj *otmp, struct monst *mon)
 {
     int tmp = 0;
     const struct permonst *ptr = mon->data;
-    boolean Is_weapon = (otmp->oclass == WEAPON_CLASS || is_weptool(otmp));
-
+    boolean Is_weapon = FALSE;
+    
+    /* not using a weapon; no special bonuses */
+    if (!otmp) return 0;
+    
+    Is_weapon = (otmp->oclass == WEAPON_CLASS || is_weptool(otmp));
+    
     if (Is_weapon)
         tmp += otmp->spe;
 
