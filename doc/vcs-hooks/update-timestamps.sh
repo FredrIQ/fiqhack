15fbff4227705ff93f187ff9fd31d2024197c6cf
Alex Smith <ais523@nethack4.org>
2013-11-18 16:52:28 +0000

    Move timestamp updating scripts into a directory of their own

--- doc/vcs-hooks/update-timestamps.sh
+++ doc/vcs-hooks/update-timestamps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Last modified by Alex Smith, 2013-09-25
+# Last modified by Alex Smith, 2013-11-18
 for x in `git ls-tree -r HEAD | cut -f 2`
 do
     sed -i -e "1,2s/Last modified by.*, ....-..-../$(git log -n 1 --pretty=format:'Last modified by %an, %at' $x | perl -pe 's/(\d+)$/`date --date=\@$1 +%F`/e')/" $x
