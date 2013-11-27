0602efc0eed72b8c59872926fcf74e7c7c99c3bd
Alex Smith <ais523@nethack4.org>
2013-11-27 22:00:42 +0000

    Fix a comment

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2013-11-16 */
+/* Last modified by Alex Smith, 2013-11-27 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -535,12 +535,12 @@ you_moved(void)
                 u.ublesscnt--;
             iflags.botl = 1;
 
-            /* One possible result of prayer is healing.  Whether or * not you
-               get healed depends on your current hit points. * If you are
-               allowed to regenerate during the prayer, the * end-of-prayer
-               calculation messes up on this. * Another possible result is
-               rehumanization, which requires * that encumbrance and movement
-               rate be recalculated. */
+            /* One possible result of prayer is healing. Whether or not you get
+               healed depends on your current hit points. If you are allowed to
+               regenerate during the prayer, the end-of-prayer calculation
+               messes up on this. Another possible result is rehumanization,
+               which requires that encumbrance and movement rate be
+               recalculated. */
             if (u.uinvulnerable) {
                 /* for the moment at least, you're in tiptop shape */
                 wtcap = UNENCUMBERED;
