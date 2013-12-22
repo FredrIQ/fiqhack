e8cb04eca089eb20a37cde229cfc81085f2326a4
Alex Smith <ais523@nethack4.org>
2013-12-22 16:18:12 +0000

    Save desync fix: shop damage
    There were two issues with this: a) it was being reversed in order
    by the save/restore process; b) it was being cleaned up by the
    save/restore process, when normally that happens on the shk's turn
    not the player's turn (which obviously causes desyncs if the shk and
    player aren't the same speed).

--- libnethack/src/restore.c
+++ libnethack/src/restore.c
@@ -131,11 +131,15 @@ restdamage(struct memfile *mf, struct level *lev, boolean ghostly)
 {
     int counter;
     struct damage *tmp_dam;
+    struct damage **pnext;
 
     counter = mread32(mf);
     if (!counter)
         return;
+
     tmp_dam = malloc(sizeof (struct damage));
+    pnext = &(lev->damagelist);
+
     while (--counter >= 0) {
         char damaged_shops[5], *shp = NULL;
 
@@ -150,6 +154,11 @@ restdamage(struct memfile *mf, struct level *lev, boolean ghostly)
         strcpy(damaged_shops,
                in_rooms(lev, tmp_dam->place.x, tmp_dam->place.y, SHOPBASE));
 
+/* TODO: Maybe we should consider doing this on level change. It definitely
+   mustn't happen on save game restore, though, otherwise saving and restoring a
+   game allows you to repair damage while the shopkeeper is asleep or
+   paralyzed, something that wouldn't happen without this code running. */
+#if 0
         for (shp = damaged_shops; *shp; shp++) {
             struct monst *shkp = shop_keeper(lev, *shp);
 
@@ -157,10 +166,12 @@ restdamage(struct memfile *mf, struct level *lev, boolean ghostly)
                 repair_damage(lev, shkp, tmp_dam, TRUE))
                 break;
         }
+#endif
 
         if (!shp || !*shp) {
-            tmp_dam->next = lev->damagelist;
-            lev->damagelist = tmp_dam;
+            tmp_dam->next = 0;
+            *pnext = tmp_dam;
+            pnext = &(tmp_dam->next);
             tmp_dam = malloc(sizeof (*tmp_dam));
         }
     }
