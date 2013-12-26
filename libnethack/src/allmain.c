d55be52570facc378f150e00330d2b56dbb75922
Alex Smith <ais523@nethack4.org>
2013-12-26 18:49:17 +0000

    Ensure lastarg is fully initialized before copying it into the save file
    This is not ideal (lastarg needs a custom save routine, not a blit),
    but it's better than nothing.

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2013-12-23 */
+/* Last modified by Alex Smith, 2013-12-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -387,7 +387,9 @@ nh_play_game(int fd)
     /* The main loop. */
     while (1) {
         char cmd[BUFSZ];
-        struct nh_cmd_arg arg;
+        /* We must initialize arg because it can end up being copied into the
+           save file. */
+        struct nh_cmd_arg arg = {0};
 
         int cmdidx;
 
