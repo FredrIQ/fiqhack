e6250a8eb3ae82254f993fc763742abeed5dd8fd
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2013-12-26 19:34:33 -0500

    Do not allow non-columned messages to wrap in menus either.

--- nethack/src/menu.c
+++ nethack/src/menu.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-10-05 */
+/* Last modified by Sean Hunt, 2013-12-26 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -151,7 +151,7 @@ draw_menu(struct gamewin *gw)
                     wmove(mdat->content, i, mdat->colpos[j + 1]);
             }
         } else
-            waddstr(mdat->content, caption);
+            waddnstr(mdat->content, caption, mdat->innerwidth);
 
         if (item->role == MI_HEADING)
             wattroff(mdat->content, settings.menu_headings);
