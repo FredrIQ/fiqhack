d38c104c252e2dfb147f6d72fa91574537d7dc9a
Alex Smith <ais523@nethack4.org>
2013-12-29 17:24:05 +0000

    Newline consistency in raw-printed error messages

--- nethack/src/windows.c
+++ nethack/src/windows.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-12-26 */
+/* Last modified by Alex Smith, 2013-12-29 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -592,7 +592,7 @@ void
 curses_raw_print(const char *str)
 {
     endwin();
-    fprintf(stderr, "%s\n", str);
+    fprintf(stderr, strchr(str, '\n') ? "%s" : "%s\n", str);
     refresh();
 
     curses_msgwin(str);
