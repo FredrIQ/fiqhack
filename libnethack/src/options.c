7d248e0b21c79ba2572e0afd0cb79d0ee0ce8343
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2013-12-30 21:18:40 -0500

    Do a redraw after changing `lit_corridor`.

--- libnethack/src/options.c
+++ libnethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-12-29 */
+/* Last modified by Sean Hunt, 2013-12-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -434,6 +434,9 @@ set_option(const char *name, union nh_optvalue value)
             return FALSE;
         }
         *bvar = option->value.b;
+
+        if (!strcmp("lit_corridor", option->name))
+            doredraw();
         return TRUE;
     } else if (!strcmp("disclose", option->name)) {
         flags.end_disclose = option->value.e;
