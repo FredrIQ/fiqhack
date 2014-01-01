e0af5db74dae036604b97f5d55e6d981a518d428
Alex Smith <ais523@nethack4.org>
2014-01-01 13:24:33 +0000

    Fix reversed test in farlook code
    It was causing "astral vision" to be shown when it shouldn't be, and
    vice versa.

--- libnethack/src/pager.c
+++ libnethack/src/pager.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2013-12-31 */
+/* Last modified by Alex Smith, 2014-01-01 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -107,7 +107,7 @@ mon_vision_summary(const struct monst *mtmp, char *outbuf)
 
     outbuf[0] = '\0';
 
-    xraydist = Xray_vision ? -1 : XRAY_RANGE * XRAY_RANGE;
+    xraydist = Xray_vision ? XRAY_RANGE * XRAY_RANGE : -1;
     /* normal vision */
     if ((mtmp->wormno ? worm_known(mtmp) : couldsee(mtmp->mx, mtmp->my) &&
          cansee(mtmp->mx, mtmp->my)) && mon_visible(mtmp) && !mtmp->minvis) {
