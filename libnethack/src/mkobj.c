2639be6362d6f8de04c014f1ff95b4f69067e293
Alex Smith <ais523@nethack4.org>
2014-01-19 22:47:10 +0000

    Timer and light source improvement from DynaHack
    Manually merged from 483d138fa690e4e7bc27a2208a49e931507a5d75,
    ae46d3e6c6c720201308721a95dfcde1b16c5684,
    f02f3e9620eddde4caed6799b9e842a827e5873f. Original change by
    Tung Nguyen

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-01-12 */
+/* Last modified by Alex Smith, 2014-01-19 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1471,8 +1471,11 @@ set_obj_level(struct level *lev, struct obj *obj)
 {
     struct obj *cobj;
 
-    transfer_timers(obj->olev, lev, obj->o_id);
-    transfer_lights(obj->olev, lev, obj->o_id);
+    if (obj->timed)
+	transfer_timers(obj->olev, lev, obj->o_id);
+    if (obj->lamplit)
+	transfer_lights(obj->olev, lev, obj->o_id);
+
     obj->olev = lev;
 
     for (cobj = obj->cobj; cobj; cobj = cobj->nobj)
