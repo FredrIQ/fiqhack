f66dbaf108ac30f548bea4bbfa3510ceb122e028
Derrick Sund <dtsund@email.arizona.edu>
2014-02-14 14:53:11 -0700

    Distinguish intrinsic protection from godly and eaten ring sources.

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2013-12-31 */
+/* Last modified by Sean Hunt, 2014-02-17 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1384,7 +1384,7 @@ eataccessory(struct obj *otmp)
         case RIN_PROTECTION:
             accessory_has_effect(otmp);
             HProtection |= FROMOUTSIDE;
-            u.ublessed += otmp->spe;
+            u.uac -= otmp->spe;
             break;
         case RIN_FREE_ACTION:
             /* Give sleep resistance instead */
