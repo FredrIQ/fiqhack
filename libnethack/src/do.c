435992a81ea2e0bec268497faf0bc4d9dff75663
Derrick Sund <dtsund@email.arizona.edu>
2014-02-19 02:58:28 -0700

    Add YAFM for entering Gehennom while hallucinating.

--- libnethack/src/do.c
+++ libnethack/src/do.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-02-11 */
+/* Last modified by Derrick Sund, 2014-02-19 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -744,8 +744,12 @@ dodown(enum u_interaction_mode uim)
         pline("Unspeakable cruelty and harm lurk down there.");
         if (yn("Are you sure you want to enter?") != 'y')
             return 0;
-        else
-            pline("So be it.");
+        else {
+            if (Hallucination)
+                pline("Please insert Disc 2.");
+            else
+                pline("So be it.");
+        }
         u.uevent.gehennom_entered = 1;  /* don't ask again */
     }
 
