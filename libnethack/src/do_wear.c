3de3c0fdfb2f476a936230233c5d146f5f0dcf4f
Derrick Sund <dtsund@email.arizona.edu>
2014-03-01 01:13:17 -0700

    Fix the amulet of restful sleep.  Closes issue 391.

--- libnethack/src/do_wear.c
+++ libnethack/src/do_wear.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-02-17 */
+/* Last modified by Derrick Sund, 2014-03-01 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -443,7 +443,7 @@ setequip(enum objslot slot, struct obj *otmp, enum equipmsg msgtype)
         }
         break;
     case AMULET_OF_RESTFUL_SLEEP:
-        if (!ESleeping && !(HSleeping & INTRINSIC))
+        if (!redundant_extrinsic && !(HSleeping & INTRINSIC))
             HSleeping = equipping ? rnd(100) : 0;
         break;
     case AMULET_OF_YENDOR:
