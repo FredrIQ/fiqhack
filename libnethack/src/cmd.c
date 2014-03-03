285962160b4f5e0cadc45b40b8a2d22cf461eb33
Derrick Sund <dtsund@email.arizona.edu>
2014-03-03 02:39:02 -0700

    Fix enlightenment leaving some dirty turnstate.

--- libnethack/src/cmd.c
+++ libnethack/src/cmd.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-03-01 */
+/* Last modified by Derrick Sund, 2014-03-03 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1085,6 +1085,10 @@ enlightenment(int final)
          */
     if (!final) {
         sprintf(buf, "%ssafely pray", can_pray(FALSE) ? "" : "not ");
+        /* can_pray sets some turnstate that needs to be reset. */
+        turnstate.pray.align = A_NONE;
+        turnstate.pray.type = pty_invalid;
+        turnstate.pray.trouble = ptr_invalid;
         if (wizard)
             sprintf(eos(buf), " (%d)", u.ublesscnt);
         you_can(&menu, buf);
