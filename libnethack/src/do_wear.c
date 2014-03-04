f7c6dd4355db302eb71e7e03e85e60a2a74b4c5c
Derrick Sund <dtsund@email.arizona.edu>
2014-03-04 02:22:43 -0700

    Don't make rings of increase damage/accuracy ID enchantment when worn.  Fixes #254.

--- libnethack/src/do_wear.c
+++ libnethack/src/do_wear.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Derrick Sund, 2014-03-01 */
+/* Last modified by Derrick Sund, 2014-03-04 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -225,7 +225,7 @@ setequip(enum objslot slot, struct obj *otmp, enum equipmsg msgtype)
     }
 
     /* Equipping armour makes its enchantment obvious. */
-    if (slot <= os_last_worn)
+    if (slot <= os_last_armor)
         o->known = TRUE;
 
     /* Side effects for all armour in the game.
