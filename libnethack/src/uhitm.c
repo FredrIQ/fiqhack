6140ba9018be0e5287b423c59217253f5b7b0b7b
Derrick Sund <dtsund@email.arizona.edu>
2014-03-08 21:46:43 -0700

    Import C343-131 (bad grammar from stealing trice corpse) fix from GruntHack.

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-03-01 */
+/* Last modified by Derrick Sund, 2014-03-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1303,7 +1303,7 @@ steal_it(struct monst *mdef, const struct attack *mattk)
             !uarmg) {
             char kbuf[BUFSZ];
 
-            sprintf(kbuf, "stolen %s corpse", mons[otmp->corpsenm].mname);
+            sprintf(kbuf, "stealing %s corpse", an(mons[otmp->corpsenm].mname));
             instapetrify(kbuf);
             break;      /* stop the theft even if hero survives */
         }
