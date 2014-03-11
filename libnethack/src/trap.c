cf8ebf1f7861ff432a0ed25798c0e724ca543d95
Derrick Sund <dtsund@email.arizona.edu>
2014-03-11 01:38:04 -0700

    Fix a compilation error introduced in a merge.

--- libnethack/src/trap.c
+++ libnethack/src/trap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-03-10 */
+/* Last modified by Derrick Sund, 2014-03-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -4015,7 +4015,6 @@ lava_effects(void)
             }
         } else
             pline("You fall into the lava!");
-        }
 
         for (obj = invent; obj; obj = obj2) {
             obj2 = obj->nobj;
