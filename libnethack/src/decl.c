2d58304ed4383fc12d80adcd651f1587ce81cdba
Derrick Sund <dtsund@email.arizona.edu>
2014-03-12 00:52:11 -0700

    Fix an improper use of memset.

--- libnethack/src/decl.c
+++ libnethack/src/decl.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-03-09 */
+/* Last modified by Derrick Sund, 2014-03-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -259,7 +259,7 @@ neutral_turnstate_tasks(void)
     memset(&zero, 0, sizeof zero);
     if (memcmp(&zeroobj, &zero, sizeof zero)) {
         impossible("zeroobj no longer zero at turn boundary");
-        memset(&zeroobj, sizeof zeroobj, 0);
+        memset(&zeroobj, 0, sizeof zeroobj);
     }
 
     log_neutral_turnstate();
