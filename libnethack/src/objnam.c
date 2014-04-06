8f8fd92c144c45f27cc9a7036f509b2a3a49eb71
Alex Smith <ais523@nethack4.org>
2014-04-06 09:49:35 +0100

    Fix unterminated msgcat_many
    I have grepped to make sure there are no more of these. Luckily,
    msgcat_many has quite a high likelihood of segfaulting when that
    happens, and the msgcat_many was in a codepath that's frequently
    run, meaning this bug was caught quickly.

--- libnethack/src/objnam.c
+++ libnethack/src/objnam.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-04-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -522,14 +522,14 @@ xname2(const struct obj *obj, boolean ignore_oquan)
                 if (un)
                     buf = msgcat_many(rock, " called ", un, NULL);
                 else
-                    buf = msgcat_many(dn, " ", rock);
+                    buf = msgcat_many(dn, " ", rock, NULL);
             } else {
                 buf = actualn;
                 if (GemStone(typ))
                     buf = msgcat(buf, " stone");
             }
         }
-break;
+        break;
 
     default:
         buf = msgprintf("glorkum %d %d %d", obj->oclass, typ, obj->spe);
