f39d02513997530e790508620d0822709139b4c4
Alex Smith <ais523@nethack4.org>
2014-04-06 04:31:04 +0100

    Fix HP check for eating acidic corpses

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-04-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1661,7 +1661,7 @@ edibility_prompts(struct obj *otmp)
             return 2;
     }
     /* HP check is needed to stop this being annoying */
-    if (cadaver && acidic(&mons[mnum]) && !Acid_resistance && u.uhp < -20) {
+    if (cadaver && acidic(&mons[mnum]) && !Acid_resistance && u.uhp < 20) {
         buf = msgprintf("%s rather acidic, and you're low on health. %s",
                         foodsmell, eat_it_anyway);
         if (yn_function(buf, ynchars, 'n') == 'n')
