e785e38ddb3575fa26c78d781687a97f1bc3655c
Alex Smith <ais523@nethack4.org>
2014-04-06 04:32:50 +0100

    Fix typo in messages when buying from an angry shk

--- libnethack/src/shk.c
+++ libnethack/src/shk.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-04-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2167,7 +2167,7 @@ speak:
         }
         buf = "\"For you, ";
         if (ANGRY(shkp))
-            buf = msgcat(buf, "scum ");
+            buf = msgcat(buf, "scum");
         else {
             static const char *const honored[5] = {
                 "good", "honored", "most gracious", "esteemed",
