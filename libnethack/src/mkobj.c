87156eb9f8ffbacc43af91abf5e7c47cd9315c8e
Alex Smith <ais523@nethack4.org>
2014-04-10 02:41:42 +0100

    Fix zeroing-out of objects
    In case you can't spot the error in the old code (it took me ages):
    the length of the object's name is being measured in units of
    sizeof (struct obj), rather than in chars, meaning that the memset
    hits random memory somewhere after the end of the object.
    
    This probably explains the random memory corruption I was seeing in
    a few specific games. Good thing Grimtooth happened to randomly
    generate on D:1 while I was testing under Valgrind, or this might
    not have been caught for months. Thanks, Valgrind! Thanks, Grimtooth!

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-04-10 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1637,14 +1637,14 @@ struct obj *
 restore_obj(struct memfile *mf)
 {
     unsigned int oflags;
-    char namelen;
+    int namelen;
     struct obj *otmp;
 
     mfmagic_check(mf, OBJ_MAGIC);
 
     namelen = mread32(mf);
     otmp = newobj(namelen, &zeroobj);
-    memset(otmp + sizeof (struct obj), 0, namelen);
+    memset(otmp, 0, namelen + sizeof (struct obj));
 
     otmp->o_id = mread32(mf);
     otmp->owt = mread32(mf);
