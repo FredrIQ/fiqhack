e44c5b72f961ea25c1656d3b86d4a582a27abb88
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-04-25 13:32:01 -0400

    Fix a silly segmentation fault.
    Fixes #433.

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-04-22 */
+/* Last modified by Sean Hunt, 2014-04-25 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -230,7 +230,8 @@ attack_checks(struct monst *mtmp,
         if (uim == uim_pacifist || uim == uim_standard) {
             if (mtmp->isshk && mtmp->mpeaceful &&
                 (ESHK(mtmp)->billct || ESHK(mtmp)->debit)) {
-                return dopay(NULL) ? ac_somethingelse : ac_cancel;
+                return dopay(&(struct nh_cmd_arg){.argtype = 0})
+                    ? ac_somethingelse : ac_cancel;
             }
             if (always_peaceful(mtmp->data) && mtmp->mpeaceful) {
                 if (mtmp->data->msound == MS_PRIEST)
