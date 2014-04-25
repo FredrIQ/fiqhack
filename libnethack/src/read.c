c485e10845e8106826121ebb38a8e8b6cbd9276a
Tung Nguyen <tungtn3@gmail.com>
2014-04-25 14:55:27 +1000

    Formal ID enchant armor scrolls used on dragon scales

--- libnethack/src/read.c
+++ libnethack/src/read.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-26 */
+/* Last modified by Alex Smith, 2014-05-05 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -675,6 +675,7 @@ seffects(struct obj *sobj, boolean * known)
                 }
                 otmp->known = 1;
                 setworn(otmp, W_MASK(os_arm));
+                *known = TRUE;
                 if (otmp->unpaid && s > 0)
                     adjust_bill_val(otmp);
                 break;
