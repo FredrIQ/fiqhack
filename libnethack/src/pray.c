5f75f39566beb7ba0f0a33275484d63af487ad27
Alex Smith <ais523@nethack4.org>
2014-04-26 06:51:42 +0100

    Allow sacrificing on neutral high altars, prevent celestial escape on unaligned high altars
    Good thing we caught /this/ one before release. (And good thing I
    happened to randomly pick a neutral altar for testing celestial
    escape on). It's crazy how much damage can be done with a single-
    letter typo.

--- libnethack/src/pray.c
+++ libnethack/src/pray.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-25 */
+/* Last modified by Alex Smith, 2014-04-26 */
 /* Copyright (c) Benson I. Margulies, Mike Stephenson, Steve Linhart, 1989. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1282,7 +1282,7 @@ dosacrifice(const struct nh_cmd_arg *arg)
     }
     /* corpse */
     if (otmp->otyp == AMULET_OF_YENDOR) {
-        if (!sanctum || altaralign == AM_NONE) {
+        if (!sanctum || altaralign == A_NONE) {
             if (Hallucination)
                 pline("You feel homesick.");
             else
