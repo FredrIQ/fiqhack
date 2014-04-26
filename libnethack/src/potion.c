24ac31b49e3805918edb207208e16eec45714922
Alex Smith <ais523@nethack4.org>
2014-04-26 06:28:17 +0100

    Grammar fixes for delayed killers

--- libnethack/src/potion.c
+++ libnethack/src/potion.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-25 */
+/* Last modified by Alex Smith, 2014-04-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -126,7 +126,7 @@ make_sick(long xtime, const char *cause, boolean talk, int type)
     if (Sick) {
         exercise(A_CON, FALSE);
         if (cause) {
-            set_delayed_killer(POISONING, cause);
+            set_delayed_killer(POISONING, killer_msg(POISONING, cause));
         } else
             set_delayed_killer(POISONING, NULL);
     } else
