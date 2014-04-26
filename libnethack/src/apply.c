21cbbca93316c64ca872c8c10a792612ec15e461
Alex Smith <ais523@nethack4.org>
2014-04-26 07:48:01 +0100

    Grammar fixes for sickness and illness

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-25 */
+/* Last modified by Alex Smith, 2014-04-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1535,7 +1535,7 @@ use_unicorn_horn(struct obj *obj)
         case 0:
             make_sick(Sick ? Sick / 3L + 1L
                            : (unsigned long)rn1(ACURR(A_CON), 20),
-                      xname(obj), TRUE, SICK_NONVOMITABLE);
+                      killer_xname(obj), TRUE, SICK_NONVOMITABLE);
             break;
         case 1:
             make_blinded(Blinded + lcount, TRUE);
--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-25 */
+/* Last modified by Alex Smith, 2014-04-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1108,11 +1108,11 @@ eatcorpse(void)
             if (Sick && (sick_time > Sick))
                 sick_time = (Sick > 1L) ? Sick - 1L : 1L;
             if (!uniq)
-                buf = msgprintf("rotted %s", corpse_xname(otmp, TRUE));
+                buf = msgprintf("a rotted %s", corpse_xname(otmp, TRUE));
             else
-                buf = msgprintf("%s%s rotted corpse",
+                buf = msgprintf("the rotted corpse of %s%s",
                                 !type_is_pname(&mons[mnum]) ? "the " : "",
-                                s_suffix(mons[mnum].mname));
+                                mons[mnum].mname);
             make_sick(sick_time, buf, TRUE, SICK_VOMITABLE);
         }
         if (carried(otmp))
