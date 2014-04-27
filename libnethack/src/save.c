92f226285c17f05a8fbd11acf87383cc66fb01a3
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-04-27 18:07:10 -0400

    Fix a crash when you cancel the ^C menu.
    0 is a valid return value; -1 indicates a failure.

--- libnethack/src/save.c
+++ libnethack/src/save.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-25 */
+/* Last modified by Sean Hunt, 2014-04-27 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -48,7 +48,7 @@ dosave(const struct nh_cmd_arg *arg)
     n = display_menu(&menu, "Do you want to stop playing?",
                      PICK_ONE, PLHINT_URGENT, &selected);
 
-    if (n)
+    if (n > 0)
         n = selected[0];
     else
         n = 3;
