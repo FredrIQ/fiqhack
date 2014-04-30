3a72f18e990fd52a371c768b2895431383bb03f5
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-04-30 14:39:14 -0400

    Fix buffer overrun due to new sentinel value COLNO.
    Also add an `isok` check to prevent this from happening again.

--- libnethack/src/teleport.c
+++ libnethack/src/teleport.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-04-28 */
+/* Last modified by Sean Hunt, 2014-04-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -880,7 +880,7 @@ rloc_to(struct monst *mtmp, int x, int y)
     if (x == mtmp->mx && y == mtmp->my) /* that was easy */
         return;
 
-    if (oldx) { /* "pick up" monster */
+    if (oldx != COLNO) { /* "pick up" monster */
         if (mtmp->wormno)
             remove_worm(mtmp);
         else {
