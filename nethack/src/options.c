e2738fc7424b703b3ed546407ac6c6542838ef6a
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-05-01 05:07:02 -0400

    Fix an incorrect direct call to `nh_get_options`
    The curses wrapper needs to be used here instead, to ensure that the client's
    options struct is returned when not in a game.

--- nethack/src/options.c
+++ nethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-10 */
+/* Last modified by Sean Hunt, 2014-05-01 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -568,7 +568,7 @@ query_new_value(struct win_menu *mdat, int idx)
     } else if (optioncopy_p) {
         if (listid != UI_OPTS) {
             curses_free_nh_opts(optlist);
-            optlist = nh_get_options();
+            optlist = curses_get_nh_opts();
             option = &optlist[id];
         }
 
