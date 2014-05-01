e7c40295f877bec3b315a1ba45ea6937e3b1ce57
Franceschini Luke <luke.a.franceschini@gmail.com>
2014-05-01 12:22:47 -0400

    Actual Fix for #59

--- libnethack/src/dokick.c
+++ libnethack/src/dokick.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Luke Franceschini, 2014-04-30 */
+/* Last modified by Luke Franceschini, 2014-05-01 */
 /* Copyright (c) Izchak Miller, Mike Stephenson, Steve Linhart, 1989. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -118,8 +118,8 @@ kickdmg(struct monst *mon, boolean clumsy, schar dx, schar dy)
                 if (mintrap(mon) == 2)
                     trapkilled = TRUE;
             }
-            if (uarmf && uarmf == KICKING_BOOTS)
-                makeknown (KICKINGBOOTS);
+            if (uarmf && uarmf->otyp == KICKING_BOOTS)
+                makeknown (KICKING_BOOTS);
         }
     }
 
