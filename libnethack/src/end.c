4010cdd35ea2d7b575de2049774e658fd5fa56ef
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-05-09 22:03:16 -0400

    Remove a useless line of code.
    This is covered by checks in the naming code for an ended game.

--- libnethack/src/end.c
+++ libnethack/src/end.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-09 */
+/* Last modified by Sean Hunt, 2014-05-09 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -817,7 +817,6 @@ display_rip(int how, long umoney, const char *killer)
 
         keepdogs(TRUE);
         /* TODO: This next line is highly dubious. */
-        viz_array[0][0] |= IN_SIGHT;    /* need visibility for naming */
         mtmp = turnstate.migrating_pets;
         pbuf = "You";
         if (mtmp) {
