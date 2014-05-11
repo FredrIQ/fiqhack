3e9111b7c1f6b0323b24985428c82e7c9facef86
Alex Smith <ais523@nethack4.org>
2014-05-11 07:19:54 +0100

    Plug a memory leak

--- nethack/src/windows.c
+++ nethack/src/windows.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-09 */
+/* Last modified by Alex Smith, 2014-05-11 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -705,6 +705,8 @@ curses_display_buffer(const char *inbuf, nh_bool trymove)
         wrap_text(COLNO - 4, linebuf, &lcount, &lines);
         for (i = 0; i < lcount; ++i)
             add_menu_txt(&menu, lines[i], MI_TEXT);
+
+        free_wrap(lines);
 
         line = strtok(NULL, "\n");
     } while (line);
