08247ddd8e9501d7c7d8905d76ef35cf48276387
Alex Smith <ais523@nethack4.org>
2014-05-24 05:53:01 +0100

    Fix a typo in a message

--- libnethack/src/wizard.c
+++ libnethack/src/wizard.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-05-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -80,7 +80,7 @@ amulet(void)
         if (!DEADMONSTER(mtmp) && mtmp->iswiz && mtmp->msleeping && !rn2(40)) {
             mtmp->msleeping = 0;
             if (distu(mtmp->mx, mtmp->my) > 2)
-                pline("get the creepy feeling that somebody noticed "
+                pline("You get the creepy feeling that somebody noticed "
                       "your taking the Amulet.");
             return;
         }
