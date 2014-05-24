64cb3dc32e127dce50da0b0671a689bc9596ab3c
Alex Smith <ais523@nethack4.org>
2014-05-24 04:12:44 +0100

    Fix another unguarded newsym

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-18 */
+/* Last modified by Alex Smith, 2014-05-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1419,7 +1419,8 @@ m_detach(struct monst *mtmp, const struct permonst *mptr)
     mtmp->dlevel->monsters[mtmp->mx][mtmp->my] = NULL;
     if (emits_light(mptr))
         del_light_source(mtmp->dlevel, LS_MONSTER, mtmp);
-    newsym(mtmp->mx, mtmp->my);
+    if (mtmp->dlevel == level)
+        newsym(mtmp->mx, mtmp->my);
     unstuck(mtmp);
     fill_pit(mtmp->dlevel, mtmp->mx, mtmp->my);
 
