cf03b1968e7bad0bb914f2c08f7e0946b6e84986
Alex Smith <ais523@nethack4.org>
2014-05-25 04:57:10 +0100

    Draw a reminder of the direction commands in the extrawin area

--- nethack/src/extrawin.c
+++ nethack/src/extrawin.c
@@ -27,6 +27,40 @@ nh_waddstr(WINDOW *win, const char *str)
     }
 }
 
+static int
+nh_waddkey(WINDOW *win, int key)
+{
+    if (!key)
+        return 0;
+
+    const char *s = friendly_keyname(key);
+    wattron(win, A_UNDERLINE);
+    wset_mouse_event(win, uncursed_mbutton_left, key,
+                     key < 256 ? OK : KEY_CODE_YES);
+    waddstr(win, s);
+    wset_mouse_event(win, uncursed_mbutton_left, 0, ERR);
+    wattroff(win, A_UNDERLINE);
+    return strlen(s);
+}
+
+static int
+classify_key(int key)
+{
+    if (key < 256)
+        return 2; /* main keyboard */
+
+    if (key == KEY_A1 || key == KEY_A2 || key == KEY_A3 ||
+        key == KEY_B1 || key == KEY_B2 || key == KEY_B3 ||
+        key == KEY_C1 || key == KEY_C2 || key == KEY_C3 ||
+        key == KEY_D1 ||                  key == KEY_D3 ||
+        key == KEY_NUMPLUS || key == KEY_NUMMINUS ||
+        key == KEY_NUMTIMES || key == KEY_NUMDIVIDE ||
+        key == KEY_PF1 || key == KEY_PF2 || key == KEY_PF3 || key == KEY_PF4)
+        return 1; /* numpad */
+
+    return 0; /* function key */
+}
+
 /* Returns TRUE if two ranges of integers have any elements in common. The
    algorithm is to find which range starts first, and then ensure it finishes
    before the other range starts. */
@@ -255,6 +289,7 @@ draw_extrawin(enum keyreq_context context)
 
     case krc_get_command:
         /* more complex than just simple hint lines will allow */
+        draw_direction_rose = 1;
         break;
 
     /* These are either fully explained, or there's no way to react to them
@@ -262,6 +297,90 @@ draw_extrawin(enum keyreq_context context)
     case krc_notification:
     case krc_keybinding:
         break;
+    }
+
+    if (draw_direction_rose && y_remaining >= 3) {
+
+        /* Draw three direction roses: function keys, keypad keys, and main
+           keyboard. First, we find out what the direction key bindings are
+           right now. (TODO: Is this too inefficient? It's of the order of 15000
+           function calls, which is quite a lot to happen on every mouse
+           movement. Perhaps we could cache it somehow.) (TODO: Handle
+           key names that overflow horizontally.) */
+        int dirkey[3][9] = {{0}};
+        int key, upkey = 0, downkey = 0, selfkey = 0;
+        int x = 8;
+        int xg;
+        int kg;
+        int i;
+        nh_bool first = TRUE;
+
+        if (context == krc_get_command) {
+            mvwaddstr(win, y + 0, 0, "Move or ");
+            mvwaddstr(win, y + 1, 0, " attack ");
+            mvwaddstr(win, y + 2, 0, " using: ");
+        }
+
+        for (key = KEY_MAX; key; key--) {
+            switch (key_to_dir(key)) {
+            case DIR_NW: dirkey[classify_key(key)][0] = key; break;
+            case DIR_N : dirkey[classify_key(key)][3] = key; break;
+            case DIR_NE: dirkey[classify_key(key)][6] = key; break;
+            case DIR_W : dirkey[classify_key(key)][1] = key; break;
+            case DIR_E : dirkey[classify_key(key)][7] = key; break;
+            case DIR_SW: dirkey[classify_key(key)][2] = key; break;
+            case DIR_S : dirkey[classify_key(key)][5] = key; break;
+            case DIR_SE: dirkey[classify_key(key)][8] = key; break;
+            case DIR_UP: upkey = key; break;
+            case DIR_DOWN: downkey = key; break;
+            case DIR_SELF: selfkey = key; break;
+            default: break;
+            }
+        }
+
+        for (kg = 0; kg < 3; kg++) {
+            int kc = 0;
+            int xmax = 0;
+            for (i = 0; i < 9; i++) kc += !!dirkey[kg][i];
+            if (i >= 4) {
+                if (first)
+                    first = FALSE;
+                else {
+                    mvwaddstr(win, y+1, x, "or");
+                    x += 3;
+                }
+                for (xg = 0; xg < 9; xg += 3) {
+                    for (i = 0; i < 3; i++) {
+                        wmove(win, y+i, x);
+                        int l = nh_waddkey(win, dirkey[kg][xg+i]);
+                        if (l > xmax)
+                            xmax = l;
+                    }
+                    x += xmax + 1;
+                }
+            }
+        }
+
+        x += 3;
+
+        if (upkey) {
+            wmove(win, y+0, x);
+            nh_waddkey(win, upkey);
+            waddstr(win, " up");
+        }
+        if (selfkey) {
+            wmove(win, y+1, x);
+            nh_waddkey(win, selfkey);
+            waddstr(win, " self");
+        }
+        if (downkey) {
+            wmove(win, y+2, x);
+            nh_waddkey(win, downkey);
+            waddstr(win, " down");
+        }
+
+        y_remaining -= 3;
+        y += 3;
     }
 
     wnoutrefresh(win);
