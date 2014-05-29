591f95f8f410c7a1e486aa42fed6f89eeb7748c1
Alex Smith <ais523@nethack4.org>
2014-05-29 02:15:18 +0100

    Remove dead WIZARD build option
    It didn't actually do anything. Fixes the remaining issues in #355.

--- libnethack/include/config.h
+++ libnethack/include/config.h
@@ -1,20 +1,11 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Alex Smith, 2014-05-29 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
 #ifndef CONFIG_H        /* make sure the compiler does not see the typedefs
                            twice */
 # define CONFIG_H
-
-/* The WIZARD option is now set via the build system. */
-# ifndef WIZARD
-#  ifdef AIMAKE_OPTION_WIZARD
-#   define STRINGIFY_EXPAND(x) STRINGIFY(x)
-#   define STRINGIFY(x) #x
-#   define WIZARD STRINGIFY_EXPAND(AIMAKE_OPTION_WIZARD)
-#  endif
-# endif
 
 /* swap byte order while reading and writing saves and bones files. This should
  * enable portability between architectures.
