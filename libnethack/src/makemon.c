0ea054cc2497094a0819ee5eca5f8b860ce3d448
Alex Smith <ais523@nethack4.org>
2014-05-30 09:46:21 +0100

    Un-daniel_t-ise level difficulty
    The formula is not identical to 3.4.3, but a lot more similar;
    monster generation is never any nastier than 3.4.3 up to D:12,
    could potentially get slightly nastier in Gehennom (not that
    anyone will care or notice) and is now identical to 3.4.3
    through the Planes.

--- libnethack/src/makemon.c
+++ libnethack/src/makemon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-24 */
+/* Last modified by Alex Smith, 2014-05-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1276,9 +1276,10 @@ rndmonst(const d_level * dlev)
         minmlev = zlevel / 6;
         /* determine the level of the strongest monster to make. The strength
            of the initial inhabitants of the level does not depend on the
-           player level. */
+           player level; instead, we assume that the player level is 1 up to
+           D:10, and dlevel - 10 thereafter (to estimate a lower bound). */
         if (in_mklev)
-            maxmlev = zlevel;
+            maxmlev = (zlevel <= 10 ? (zlevel + 1) / 2 : zlevel - 5);
         else
             maxmlev = (zlevel + u.ulevel) / 2;
         upper = Is_rogue_level(dlev);
