7ca0627b7437e367c70f0559fc44111c30b15434
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-06-03 12:58:40 -0400

    Fix typo.

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-31 */
+/* Last modified by Sean Hunt, 2014-06-03 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -472,7 +472,7 @@ just_reloaded_save:
 
     if (replay_forced) {
         pline("This game ended while you were loading it.");
-        pline("Loading in replay mode intead.");
+        pline("Loading in replay mode instead.");
         replay_forced = FALSE;
     }
 
