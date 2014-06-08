d6d270c0f3b7b4f0f71a4f763f383c051e862291
Derrick Sund <dtsund@email.arizona.edu>
2014-06-08 11:54:38 -0700

    Fix some inverted logic preventing branch stairs from being placed at all.

--- libnethack/src/mklev.c
+++ libnethack/src/mklev.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-24 */
+/* Last modified by Derrick Sund, 2014-06-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1019,7 +1019,7 @@ place_branch(struct level *lev, branch * br,    /* branch to place */
     if (!br || made_branch || Is_stronghold(&lev->z))
         return;
 
-    if (x == COLNO) {   /* find random coordinates for branch */
+    if (x != COLNO) {   /* find random coordinates for branch */
         br_room = find_branch_room(lev, &m);
         x = m.x;
         y = m.y;
