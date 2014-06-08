50cc5d9cf39203c8f5b51e3147cab53520959837
Derrick Sund <dtsund@email.arizona.edu>
2014-06-08 15:56:20 -0700

    Don't print tabs to the message buffer; replace them with spaces.

--- libnethack/src/pline.c
+++ libnethack/src/pline.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-05-29 */
+/* Last modified by Derrick Sund, 2014-06-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -71,6 +71,17 @@ vpline(boolean nonblocking, boolean norepeat, const char *line,
         return;
 
     pbuf = msgvprintf(line, the_args, TRUE);
+
+    /* Remove all tabs from the string and replace them with spaces.  Some
+       messages generated from menu use will have formatting tabs in them, but
+       these look ugly in the message buffer (and also mess with the wrapping
+       logic). */
+    char *untabber = (char*)pbuf;
+    while (*untabber != '\0') {
+        if (*untabber == '\t')
+            *untabber = ' ';
+        untabber++;
+    }
 
     line = pbuf;
 
