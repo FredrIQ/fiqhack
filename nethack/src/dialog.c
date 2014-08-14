8771d1b602666bf76b6b47ddf95487b648e78d93
Alex Smith <ais523@nethack4.org>
2014-08-14 13:35:15 +0100

    Fix off-by-one in buffer allocation in curses_yn_function
    The previous code happened to work on Linux due to the order in which
    the stack is allocated (not that we should rely on that), but crashes
    on OS X, which is better at catching this sort of buffer overflow.
    
    Memcheck can't catch this sort of error. I guess we'll have to wait
    for sgcheck to become more mature (it currently can't handle NetHack
    4) before we can get automated detection of this happening.

--- nethack/src/dialog.c
+++ nethack/src/dialog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-05 */
+/* Last modified by Alex Smith, 2014-08-14 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -214,7 +214,11 @@ char
 curses_yn_function(const char *query, const char *resp, char def)
 {
     int key;
-    char prompt[strlen(query) + strlen(resp) + 8];
+    /*
+     * "query [resp] (y) \0"
+     *       12    3456789
+     */
+    char prompt[strlen(query) + strlen(resp) + 9];
     char *rb;
     char respbuf[strlen(resp) + 1];
 
