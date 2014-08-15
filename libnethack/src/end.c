2a55f29cac04fb41ba7958e657cf6ee1163e8fa7
Alex Smith <ais523@nethack4.org>
2014-08-15 13:50:50 +0100

    Remove a comment that no longer applies

--- libnethack/src/end.c
+++ libnethack/src/end.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-06-30 */
+/* Last modified by Alex Smith, 2014-08-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -796,7 +796,6 @@ display_rip(int how, long umoney, const char *killer)
         int i;
 
         keepdogs(TRUE);
-        /* TODO: This next line is highly dubious. */
         mtmp = turnstate.migrating_pets;
         pbuf = "You";
         if (mtmp) {
