f74aca7b2627ff0137c5a603db19a72c02186aae
Alex Smith <ais523@nethack4.org>
2014-08-17 04:12:15 +0100

    Fix a typo that prevents the code building

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-16 */
+/* Last modified by Alex Smith, 2014-08-17 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1013,7 +1013,7 @@ use_candle(struct obj **optr)
 boolean
 snuff_candle(struct obj * otmp)
 {       /* call in drop, throw, and put in box, etc. */
-    if (!obj) {
+    if (!otmp) {
         impossible("snuffing null object");
         return FALSE;
     }
