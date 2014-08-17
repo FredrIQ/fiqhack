643b6e0a2ff901ed954ca8da30b147cf2937dbce
Alex Smith <ais523@nethack4.org>
2014-08-17 23:21:57 +0100

    Fix an off-by-one in screen layout
    Required to make the options menu visible in 80x24.

--- nethack/src/windows.c
+++ nethack/src/windows.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-16 */
+/* Last modified by Alex Smith, 2014-08-17 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -628,10 +628,10 @@ newwin_onscreen(int ysize, int xsize, int ymin, int xmin)
         ymin = 0;
     if (xmin < 0)
         xmin = 0;
-    if (ysize + ymin >= LINES)
-        ymin = LINES - ysize - 1;
-    if (xsize + xmin >= COLS)
-        xmin = COLS - xsize - 1;
+    if (ysize + ymin > LINES)
+        ymin = LINES - ysize;
+    if (xsize + xmin > COLS)
+        xmin = COLS - xsize;
     return newwin(ysize, xsize, ymin, xmin);
 }
 
