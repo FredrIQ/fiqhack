6e19ca06bd2bafaadb35dfbe81654b6006c2ec4c
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-08-28 18:51:58 -0400

    Allow using a wielded towel.
    Fixes #591 (C343-274).

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-17 */
+/* Last modified by Sean Hunt, 2014-08-28 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -96,7 +96,7 @@ use_towel(struct obj *obj)
     if (!freehand()) {
         pline("You have no free %s!", body_part(HAND));
         return 0;
-    } else if (obj->owornmask) {
+    } else if (obj->owornmask & W_WORN) {
         pline("You cannot use it while you're wearing it!");
         return 0;
     } else if (obj->cursed) {
