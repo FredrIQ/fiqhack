4d888268a9dca70aa1d986d3004259e5f0c5f97b
Alex Smith <ais523@nethack4.org>
2014-09-01 11:20:49 +0100

    Timestamp updates

--- nethack/include/nhcurses.h
+++ nethack/include/nhcurses.h
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-16 */
+/* Last modified by Alex Smith, 2014-09-01 */
 /* Copyright (c) Daniel Thaler, 2011                              */
 /* NetHack may be freely redistributed.  See license for details. */
 
