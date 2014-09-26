fcb9b31e2ce10e7e9ca8552f34929f2a55cfbc27
Alex Smith <ais523@nethack4.org>
2014-09-26 20:46:44 +0100

    Avoid useless prompt one screen after DYWYPI

--- libnethack/src/end.c
+++ libnethack/src/end.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-08-20 */
+/* Last modified by Alex Smith, 2014-09-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -300,7 +300,7 @@ disclose(int how, boolean taken, long umoney)
                     discover_object(obj->otyp, TRUE, FALSE, TRUE);
                     obj->known = obj->bknown = obj->dknown = obj->rknown = 1;
                 }
-                display_inventory(NULL, TRUE);
+                display_inventory(NULL, FALSE);
                 container_contents(invent, TRUE, TRUE);
             }
             if (c == 'q')
