1c6e0758b5f8b029063f335a9effafa9a54c47ba
Alex Smith <ais523@nethack4.org>
2014-09-30 21:57:25 +0100

    Fix crash in makeplural()
    I'd left it a little too close to the vanilla code, which was for
    some reason trying very hard to not do subtractions. This meant
    that there were some pointer-into-deallocated-memory bugs
    (technically, mortalized rather than deallocated, but the memory
    nonetheless had the wrong contents, which is why it was
    segfaulting).

--- libnethack/src/objnam.c
+++ libnethack/src/objnam.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-08-17 */
+/* Last modified by Alex Smith, 2014-09-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1480,18 +1480,20 @@ makeplural(const char *str)
             || !strncmp(spot, " du ", 4)) {
             suffix = spot;
             len = spot - str;
-            str = msgchop(str, spot - str);
+            str = msgchop(str, len);
             spot = str + len;
             break;
         }
     }
     spot--;
     while (*spot == ' ') {
-        len--;
         spot--; /* Strip blanks from end */
     }
     spot++;
-    str = msgchop(str, spot - str);
+    len = spot - str;
+    str = msgchop(str, len);
+    spot = str + len;
+
     /* spot is now the trailing NUL */
 
     /* Single letters and nonletters pluralize with 's. */
