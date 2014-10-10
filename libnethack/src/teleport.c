f2f91a0405f92b8f85de69fedb0b9733e20ba099
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-10-10 17:51:37 -0400

    Reserve wizmode teleport for wizmode.
    Fixes #655.

--- libnethack/src/teleport.c
+++ libnethack/src/teleport.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-10-08 */
+/* Last modified by Sean Hunt, 2014-10-10 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -525,7 +525,7 @@ dotele(const struct nh_cmd_arg *arg)
             pline("You shudder for a moment.");
     }
 
-    if (!tele_impl(TRUE, TRUE))
+    if (!tele_impl(FALSE, TRUE))
         return 0;
     next_to_u();
 
