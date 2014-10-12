64a2474b6c0b42b3b5f9f214813720a8230a843d
Alex Smith <ais523@nethack4.org>
2014-10-12 16:23:17 +0100

    Bugfix: negative bytes in a PNG chunk size

--- nethack/src/windows.c
+++ nethack/src/windows.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-10-11 */
+/* Last modified by Alex Smith, 2014-10-12 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -58,7 +58,7 @@ static size_t
 seek_tile_file(FILE *in) {
 
     size_t filelen;
-    char header[12];
+    unsigned char header[12];
 
     fseek(in, 0, SEEK_END);
     filelen = ftell(in);
