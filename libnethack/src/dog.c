33d1b3b2b5db4e701ab3071845435c10ac2e744d
Alex Smith <ais523@nethack4.org>
2014-10-15 05:58:18 +0100

    Obvious warnings fixes

--- libnethack/src/dog.c
+++ libnethack/src/dog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-10-13 */
+/* Last modified by Alex Smith, 2014-10-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -250,7 +250,7 @@ mon_arrive(struct monst *mtmp, boolean with_you)
     xlocale = mtmp->mtrack[1].x;
     ylocale = mtmp->mtrack[1].y;
     mtmp->mtrack[0].x = mtmp->mtrack[1].x = COLNO;
-    mtmp->mtrack[1].y = mtmp->mtrack[1].y = ROWNO;
+    mtmp->mtrack[0].y = mtmp->mtrack[1].y = ROWNO;
 
     for (otmp = mtmp->minvent; otmp; otmp = otmp->nobj)
         set_obj_level(mtmp->dlevel, otmp);
