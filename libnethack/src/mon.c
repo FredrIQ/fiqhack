44e5bea0294d8655eef9cbc38b5aec67510f211c
Alex Smith <ais523@nethack4.org>
2014-10-15 05:48:32 +0100

    Fix invalid coordinates when a shrieker shrieks

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Sean Hunt, 2014-10-08 */
+/* Last modified by Alex Smith, 2014-10-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2130,9 +2130,9 @@ m_respond(struct monst *mtmp)
         }
         if (!rn2(10)) {
             if (!rn2(13))
-                makemon(&mons[PM_PURPLE_WORM], level, ROWNO, COLNO, NO_MM_FLAGS);
+                makemon(&mons[PM_PURPLE_WORM], level, COLNO, ROWNO, NO_MM_FLAGS);
             else
-                makemon(NULL, level, ROWNO, COLNO, NO_MM_FLAGS);
+                makemon(NULL, level, COLNO, ROWNO, NO_MM_FLAGS);
 
         }
         aggravate();
