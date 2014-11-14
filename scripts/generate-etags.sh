7fa48bafd6d006dd6a938888380eae57eb78b1d1
Alex Smith <ais523@nethack4.org>
2014-11-14 08:12:10 +0000

    Leave out libjansson when generating etags
    I'm surprised it was this simple to exclude it, but it has a different
    directory structure.

--- scripts/generate-etags.sh
+++ scripts/generate-etags.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Last modified by Alex Smith, 2013-11-27
+# Last modified by Alex Smith, 2014-11-14
 
 # Generate a TAGS file for use with Emacs.
 # Run from the root of the distribution, not the scripts folder.
-etags */*.[ch] */*/*.[ch] */*/*/*.[ch]
+etags */*/*.[ch] */*/*/*.[ch]
