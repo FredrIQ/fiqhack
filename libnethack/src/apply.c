5c6659151ade65e3462380bec23170583a883c57
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2014-11-20 14:38:02 -0500

    Ticket #728: make tinnable() do a sanity-check whether corpse is indeed a corpse.
    It seems clear from the argument name that the original intention was
    that only corpses should ever be passed as an argument, but in
    practice the function was being called to determine what _objects_
    were reasonable to apply the tinning kit to, which resulted in being
    able to tin things that weren't even food.
    
    It has been suggested that it should also be possible to tin other
    things, e.g., kelp.  (Fruits and vegetables seem likewise obvious.)
    I have not attempted to implement that at this time, since it would
    be much more involved and would add kinds of tins that do not exist
    in vanilla.  Perhaps we could revisit that later, but for now this
    just (hopefully) fixes the bug.
    
    Lightly tested:  with this change, I can still tin a corpse off
    the floor, but with other objects (e.g., rations, scrolls) on the
    floor at my feet, the game tells me I have nothing to tin.
    
    I have not done a full audit of every place tinnable() is called.

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1451,6 +1451,8 @@ jump(const struct nh_cmd_arg *arg, int magic)
 boolean
 tinnable(const struct obj * corpse)
 {
+    if (!(corpse->otyp == CORPSE)) /* Originally the caller was expected */
+        return 0;                  /* to check this, but that caused bugs. */
     if (corpse->oeaten)
         return 0;
     if (!mons[corpse->corpsenm].cnutrit)
