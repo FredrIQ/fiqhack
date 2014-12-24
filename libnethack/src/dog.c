78cd7777827e85d26ef29e9a4a12041b592b7776
Sean Hunt <scshunt@csclub.uwaterloo.ca>
2014-12-24 13:50:37 -0700

    Properly initialize mu[xy].
    This shouldn't break, but it might. But the current behaviour is very wrong.

--- libnethack/src/dog.c
+++ libnethack/src/dog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-11-22 */
+/* Last modified by Sean Hunt, 2014-12-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -396,8 +396,8 @@ mon_arrive(struct monst *mtmp, boolean with_you)
         }
     }
 
-    mtmp->mux = mtmp->mx;
-    mtmp->muy = mtmp->my;
+    mtmp->mux = COLNO;
+    mtmp->muy = ROWNO;
 }
 
 /* heal monster for time spent elsewhere */
