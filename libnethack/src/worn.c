072da4cc182637b00a39f5bed00f9f033b0c22b2
Alex Smith <ais523@nethack4.org>
2015-02-12 04:43:24 +0000

    Another vision-calculations-during-level-creation bugfix

--- libnethack/src/worn.c
+++ libnethack/src/worn.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-02-10 */
+/* Last modified by Alex Smith, 2015-02-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -631,7 +631,8 @@ mon_break_armor(struct monst *mon, boolean polyspot)
     boolean show_msg = (lev == level);
     boolean vis = (lev == level && cansee(mon->mx, mon->my));
     boolean handless_or_tiny = (nohands(mdat) || verysmall(mdat));
-    const char *pronoun = mhim(mon), *ppronoun = mhis(mon);
+    const char *pronoun = vis ? mhim(mon) : NULL,
+        *ppronoun = vis ? mhis(mon) : NULL;
 
     if (breakarm(mdat)) {
         if ((otmp = which_armor(mon, os_arm)) != 0) {
