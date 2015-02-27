b95ba0eed0041f2fc3fb6cb711dc9115ffebb4ab
Alex Smith <ais523@nethack4.org>
2015-02-27 03:38:16 +0000

    Let unskilled/uncursed monster detection linger for a few turns
    To give ASCII players an opportunity to farlook the monsters that
    they just detected. This doesn't remove the map pause, because it'd
    be easy to forget to do the farlooking otherwise, and players might
    not think of going into replay mode to correct their mistake.
    
    Fixes #171.

--- libnethack/src/detect.c
+++ libnethack/src/detect.c
@@ -570,6 +570,11 @@ monster_detect(struct obj *otmp,        /* detecting object (if any) */
     struct monst *mtmp;
     int mcnt = 0;
 
+    /* Change for NetHack 4.3-beta2: this gives a lingering effect for a couple
+       of turns after the detection. Mostly, this is so that ASCII players who
+       don't use the mouse can get an opportunity to farlook the monsters they
+       detected. */
+    incr_itimeout(&HDetect_monsters, 3);
 
     /* Note: This used to just check level->monlist for a non-zero value but in 
        versions since 3.3.0 level->monlist can test TRUE due to the presence of 
