1f52b5afb6c5cb6d4f834b63572facdc6ce66060
Alex Smith <ais523@nethack4.org>
2015-03-21 23:09:03 +0000

    Don't crash when a wielded wand is destroyed via lightning
    Found via the testsuite.

--- libnethack/src/do_wear.c
+++ libnethack/src/do_wear.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-03-13 */
+/* Last modified by Alex Smith, 2015-03-21 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -623,8 +623,8 @@ setequip(enum objslot slot, struct obj *otmp, enum equipmsg msgtype)
 void
 setunequip(struct obj *otmp)
 {
-    if (otmp->owornmask & W_EQUIP)
-        setequip(objslot_from_mask(otmp->owornmask & W_EQUIP),
+    if (otmp->owornmask & W_WORN)
+        setequip(objslot_from_mask(otmp->owornmask & W_WORN),
                  NULL, em_silent);
 }
 
