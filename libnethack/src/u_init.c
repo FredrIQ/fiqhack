41644bc07814f051eda3bf82c587790c935926e1
Alex Smith <ais523@nethack4.org>
2015-04-01 17:20:21 +0100

    Fix probabilities of Barbarian starting equipment

--- libnethack/src/u_init.c
+++ libnethack/src/u_init.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-03-22 */
+/* Last modified by Alex Smith, 2015-04-01 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -619,7 +619,7 @@ u_init_inv_skills(void)
         break;
     case PM_BARBARIAN:
         trobj_list = copy_trobj_list(Barbarian);
-        if (!rolern2(100)) {
+        if (!rolern2(2)) {
             trobj_list[B_MAJOR].trotyp = BATTLE_AXE;
             trobj_list[B_MINOR].trotyp = SHORT_SWORD;
         }
