b1b94e1b522c2d06776582984a14a4f6bf1f5183
Alex Smith <ais523@nethack4.org>
2015-07-12 05:03:23 +0100

    Change "Time penalty" to "Turncount" in a score breakdown
    Just cosmetic, but may be less misleading.
    
    Fixes #890.

--- libnethack/src/end.c
+++ libnethack/src/end.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-03-19 */
+/* Last modified by Alex Smith, 2015-07-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -668,7 +668,7 @@ calc_score(int how, boolean show, long umoney)
 
     if (show) {
         buf = msgprintf(
-            "Time penalty:    %10ld turn%s              (%5ld points)",
+            "Turncount:       %10ld turn%s              (%5ld points)",
             category_raw, category_raw == 1 ? " " : "s", -category_points);
         add_menutext(&menu, buf);
     }
