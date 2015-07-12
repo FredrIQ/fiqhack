8535392d914a2632003fe1bf6b52dfed1078702b
Alex Smith <ais523@nethack4.org>
2015-07-12 13:17:02 +0100

    Respect ALLOW_ALL in the list at a "*" inventory prompt
    Fixes #874, mostly. We might want to change the status of gold in
    the "?" side of the prompt, but that's a matter of opinion; this
    was an outright bug.

--- libnethack/src/invent.c
+++ libnethack/src/invent.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-03-23 */
+/* Last modified by Alex Smith, 2015-07-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -969,6 +969,17 @@ getobj(const char *let, const char *word, boolean isarg)
             case OBJECT_USABLE:
                 *ap++ = *bp++ = otmp->invlet;
                 buflen++;
+                break;
+            }
+        } else if (allowall) {
+            switch (object_selection_checks(otmp, word)) {
+            case CURRENTLY_NOT_USABLE:
+            case IMPOSSIBLE_USE:
+                break;
+
+            case OBJECT_USABLE:
+            case NONSENSIBLE_USE:
+                *ap++ = otmp->invlet;
                 break;
             }
         }
