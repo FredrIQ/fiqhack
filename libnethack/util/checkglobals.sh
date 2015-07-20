b1773ac3a999e0c4d741636573e286938ae00081
Alex Smith <ais523@nethack4.org>
2015-07-20 18:41:29 +0100

    Hide initialized data from the checkglobals output
    It's nearly all read-only, but not placed into the read-only data
    section for some reason.  This means that there are way too many
    false positives.
    
    Luckily, most of the problematic globals are in the /uninitialized/
    data section, so checkglobals still gives us plenty to work with.

--- libnethack/util/checkglobals.sh
+++ libnethack/util/checkglobals.sh
@@ -2,7 +2,7 @@
 # Last modified by Alex Smith, 2015-07-20
 # Copyright (C) 2013, 2015 Alex Smith
 # NetHack may be freely redistributed. See license for details.
-for x in `nm "$1" | egrep -v '^[0-9a-f]* *([tTrUwA]|d __)' | sort -k1.20 | cut -c20-`
+for x in `nm "$1" | egrep -v '^[0-9a-f]* *[tTrUwAd]' | sort -k1.20 | cut -c20-`
 do
     echo "warning: found static or global variable $x" 1>&2
 done
