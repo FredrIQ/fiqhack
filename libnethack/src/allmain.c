b3289093150f9723ca2eda955b948472e8cfd897
Alex Smith <ais523@nethack4.org>
2015-07-25 17:50:44 +0100

    Bypass handling should run per-action, not per-turn
    Fixes a vanilla bug; thanks to the desync detector for noticing it.
    (To reproduce in vanilla: get multiple actions in a turn; polymorph
    a monster on the first of those actions, causing its armour to fall
    off; attempt to polypile the armour that fell off on the second of
    those actions; the polymorph beam won't affect the armour.)

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-07-20 */
+/* Last modified by Alex Smith, 2015-07-25 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -826,8 +826,6 @@ you_moved(void)
             /* most turn boundary effects should be below here */
             /***************************************************/
 
-            if (flags.bypasses)
-                clear_bypasses();
             if (Glib)
                 glibr();
             nh_timeout();
@@ -1404,6 +1402,12 @@ command_input(int cmdidx, struct nh_cmd_arg *arg)
 
     if (turnstate.vision_full_recalc)
         vision_recalc(0);       /* vision! */
+
+    /* We must do this /before/ the monsters get their turn, to prevent bypass
+       flags persisting from one action to the next. (TODO: The bypass system
+       could really do with an overhaul.) */
+    if (flags.bypasses)
+        clear_bypasses();
 
     if (didmove)
         you_moved();
