d1a7fe2f76c35fc7eeac733ef21ba18a965b8460
FIQ <fredde1994@gmail.com>
2015-09-14 00:14:48 +0200

    Fixed a bug where free turns could potentially cause too high catchup counters. This could cause issues like tame monsters going hostile very quickly, things rapidly dying from hunger, and fun stuff like that.
    Fixes #951.
    
    Cherry picked from commit 4cd95502687631369b59ac3ce6152ee00d4b0ca6.
    
    Conflicts:
    	libnethack/src/allmain.c

--- libnethack/src/dog.c
+++ libnethack/src/dog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2015-07-20 */
+/* Last modified by Alex Smith, 2015-10-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -423,6 +423,8 @@ mon_catchup_elapsed_time(struct monst *mtmp, long nmv)
         imv = LARGEST_INT - 1;
     else
         imv = (int)nmv;
+    if (!imv)
+        return;
 
     /* might stop being afraid, blind or frozen */
     /* set to 1 and allow final decrement in movemon() */
