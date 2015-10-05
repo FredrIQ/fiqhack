b6e8a0e0a06cbe506efe0b50c0d0cd72369644f2
Fredrik Ljungdahl <fredde1994@gmail.com>
2015-10-05 09:41:56 +0200

    * Fixed accidentally reversing the nonliving check for wearing "oLS

--- libnethack/src/worn.c
+++ libnethack/src/worn.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2015-10-04 */
+/* Last modified by Fredrik Ljungdahl, 2015-10-05 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -250,7 +250,7 @@ m_dowear_type(struct monst *mon, enum objslot slot, boolean creation,
                  obj->otyp != AMULET_OF_REFLECTION))
                 continue;
             if (obj->otyp == AMULET_OF_LIFE_SAVING &&
-                !nonliving(mon->data))
+                nonliving(mon->data))
                 continue;
             best = obj;
             goto outer_break;   /* no such thing as better amulets */
