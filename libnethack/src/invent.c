bb9649322cc68354dc3f55e08f64042667f2e8fd
Fredrik Ljungdahl <fredde1994@gmail.com>
2015-11-04 03:28:56 +0100

    Minor bug fixes for monster identify
    * Monsters no longer consider the identification object itself while considering unIDed objects to possibly identify
    * You no longer see "X has already identified all of his/her possessions" unless you see the monster

--- libnethack/src/invent.c
+++ libnethack/src/invent.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2015-11-01 */
+/* Last modified by Fredrik Ljungdahl, 2015-11-04 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1265,7 +1265,7 @@ identify_pack(struct monst *mon, int id_limit)
     if (!unid_cnt) {
         if (you)
             pline("You have already identified all of your possessions.");
-        else
+        else if (vis)
             pline("%s has already identified all of %s possessions.",
                   Monnam(mon), mhis(mon));
     } else if (!id_limit) {
--- libnethack/src/muse.c
+++ libnethack/src/muse.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2015-11-03 */
+/* Last modified by Fredrik Ljungdahl, 2015-11-04 */
 /* Copyright (C) 1990 by Ken Arromdee                              */
 /* NetHack may be freely redistributed.  See license for details.  */
 
@@ -1667,7 +1667,8 @@ find_item_single(const struct monst *mon, struct obj *obj, boolean spell,
     if (otyp == SPE_IDENTIFY ||
         otyp == SCR_IDENTIFY)
         for (otmp = mon->minvent; otmp; otmp = otmp->nobj)
-            if (otmp->otyp != GOLD_PIECE && (!otmp->mknown || !otmp->mbknown))
+            if (otmp->otyp != GOLD_PIECE && (!otmp->mknown || !otmp->mbknown) &&
+                otmp != obj)
                 return 1;
 
     /* Defensive only */
