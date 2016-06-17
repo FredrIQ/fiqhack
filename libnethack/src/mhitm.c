2c9dd17fe7d36b1da541280710efd97a9342adc7
Fredrik Ljungdahl <fredde1994@gmail.com>
2016-06-17 15:24:20 +0200

    Fixed mistakes with gaze reflection messages

--- libnethack/src/mhitm.c
+++ libnethack/src/mhitm.c
@@ -679,12 +679,12 @@ gazemm(struct monst *magr, struct monst *mdef, const struct attack *mattk)
             if (vis)
                 mon_reflects(mdef, magr, FALSE,
                              "%s gaze is reflected by %s %s.",
-                             uagr ? s_suffix(Monnam(magr)) : "Your");
+                             udef ? "Your" : s_suffix(Monnam(mdef)));
             if (reflecting(magr)) {
                 if (vis)
                     mon_reflects(magr, mdef, TRUE, 
                                  "%s gaze is reflected further by %s %s!",
-                                 uagr ? s_suffix(Monnam(magr)) : "Your");
+                                 uagr ? "Your" : s_suffix(Monnam(magr)));
                 break;
             }
             if (!visad) { /* probably you're invisible */
