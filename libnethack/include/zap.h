fe83461272ee23eb4eeb2fb164a60d9dfcb93fff
Fredrik Ljungdahl <fredde1994@gmail.com>
2016-07-05 23:50:58 +0200

    Add copyright stuff mistakenly left out of zap.h earlier

--- libnethack/include/zap.h
+++ libnethack/include/zap.h
@@ -1,3 +1,8 @@
+/* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
+/* Last modified by Fredrik Ljungdahl, 2017-11-27 */
+/* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
+/* NetHack may be freely redistributed.  See license for details. */
+
 #ifndef ZAP_H
 # define ZAP_H
 
