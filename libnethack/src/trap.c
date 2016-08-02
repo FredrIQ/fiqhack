8304db0d63bb2b3635128d5d3df6100c2b78f9b8
Fredrik Ljungdahl <fredde1994@gmail.com>
2016-08-02 18:30:34 +0200

    Fix "You attempt a teleport spell!" being broken

--- libnethack/src/trap.c
+++ libnethack/src/trap.c
@@ -3217,9 +3217,8 @@ drown(void)
         turnstate.vision_full_recalc = TRUE;
         return FALSE;
     }
-    /* TODO: Isn't this u_helpless check backwards? */
     if ((Teleportation) &&
-        u_helpless(hm_unconscious) && (Teleport_control || rn2(3) < Luck + 2)) {
+        !u_helpless(hm_unconscious) && (Teleport_control || rn2(3) < Luck + 2)) {
         pline(msgc_consequence,
               "You attempt a teleport spell."); /* utcsri!carroll */
         if (!level->flags.noteleport) {
