3cb2e7c2ae6a53512263b865eb5602d41c809ca3
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-04-24 05:03:53 +0200

    Simplify newt eating code

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -574,30 +574,27 @@ cpostfx(struct monst *mon, int pm)
     switch (pm) {
     case PM_NEWT:
         /* MRKR: "eye of newt" may give small magical energy boost */
-        if (you ||
-            (!you && mon->mspec_used > rn2(5))) {
-            if (you) {
-                int old_uen = u.uen;
-                msgc = msgc_statusheal;
+        if (you) {
+            int old_uen = u.uen;
+            msgc = msgc_statusheal;
 
-                u.uen += 1 + rn2_on_rng(3, rng_newt_pw_boost);
-                if (u.uen > u.uenmax) {
-                    u.uenmax++;
-                    u.uen = u.uenmax;
-                } else
-                    u.uenmax++;
+            u.uen += 1 + rn2_on_rng(3, rng_newt_pw_boost);
+            if (u.uen > u.uenmax) {
+                u.uenmax++;
+                u.uen = u.uenmax;
+            } else
+                u.uenmax++;
 
-                if (you)
-                    msgc = msgc_intrgain;
+            if (you)
+                msgc = msgc_intrgain;
 
-                if (old_uen != u.uen)
-                    pline(msgc, "You feel a mild buzz.");
-            } else {
-                mon->mspec_used -= min(mon->mspec_used, rnd(3));
-                if (vis)
-                    pline(msgc, "You sense a mild buzz coming from %s.",
-                          mon_nam(mon));
-            }
+            if (old_uen != u.uen)
+                pline(msgc, "You feel a mild buzz.");
+        } else if (mon->mspec_used > rn2(5)) {
+            mon->mspec_used -= min(mon->mspec_used, rnd(3));
+            if (vis)
+                pline(msgc, "You sense a mild buzz coming from %s.",
+                      mon_nam(mon));
         }
         break;
     case PM_WRAITH:
