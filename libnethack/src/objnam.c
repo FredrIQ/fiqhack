a62ecffd93ae27ebe8cc8e699e983412b8c57499
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-05-30 11:52:12 +0200

    Pluralize virus correctly

--- libnethack/src/objnam.c
+++ libnethack/src/objnam.c
@@ -1656,6 +1656,7 @@ static const char *plurals_dictionary[][2] = {
     {"hypha", "hyphae"},
     {"larva", "larvae"},
     {"lotus", "lotuses"},     /* -us exception */
+    {"virus", "viruses"},     /* -us exception */
     {"wumpus", "wumpuses"},   /* -us exception */
     {"^bus", "buses"},        /* -us exception */
     {"us", "i"},              /* e.g. "fungus" */
