1be29ef27ebb7284f9cacbd3a6accb2b806f0cb4
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-06-10 21:58:14 +0200

    Fix another bug

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -314,7 +314,7 @@ bhitm(struct monst *magr, struct monst *mdef, struct obj *otmp, int range)
     case WAN_TELEPORTATION:
     case SPE_TELEPORT_AWAY:
         known = TRUE;
-        if ((wandlevel >= P_EXPERT || selfzap) && tele_restrict(mdef))
+        if ((wandlevel < P_EXPERT || selfzap) && tele_restrict(mdef))
             break; /* noteleport */
         if (level->flags.noteleport) {
             /* master proficiency can bypass noteleport */
