ff84e794e7c68f1c2484942fadf0f88239f8286e
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-09-02 22:26:49 +0200

    Minor fix: only appraise weapons if you don't already know it (prevents msg spam)

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -725,7 +725,7 @@ hmon_hitmon(struct monst *mon, struct obj *obj, int thrown)
         /* [this assumes that `!thrown' implies wielded...] */
         wtype = thrown ? weapon_type(wep) : uwep_skill_type();
         use_skill(wtype, 1);
-        if (P_SKILL(wtype) >= P_BASIC && wep &&
+        if (wep && !wep->known && P_SKILL(wtype) >= P_BASIC &&
             (P_SKILL(wtype) >= P_EXPERT ||
              !rn2(P_SKILL(wtype) == P_SKILLED ? 10 : 100))) {
             pline(msgc_info,
