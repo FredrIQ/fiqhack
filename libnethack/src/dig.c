6c582859f36780c3f407370db61946105591a0af
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-09-24 00:08:22 +0200

    Fix a bug where digging wasn't handled correctly

--- libnethack/src/dig.c
+++ libnethack/src/dig.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2015-11-17 */
+/* Last modified by Fredrik Ljungdahl, 2017-09-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -519,7 +519,7 @@ digactualhole(int x, int y, struct monst *mon, int ttyp)
     boolean vis = (mon && canseemon(mon));
     boolean byobj = !mon;
     struct level *lev = (byobj ? level : m_dlevel(mon));
-    struct monst *target = m_at(lev, x, y);
+    struct monst *target = um_at(lev, x, y);
     boolean hityou = (target == &youmonst);
     struct obj *oldobjs, *newobjs;
     struct trap *ttmp;
