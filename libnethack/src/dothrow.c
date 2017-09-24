fb97812ef15a714db6b28b63bbab4e476cfe011e
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-09-24 16:50:44 +0200

    Fix a crash caused by autoswap in some cases

--- libnethack/src/dothrow.c
+++ libnethack/src/dothrow.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2017-06-29 */
+/* Last modified by Fredrik Ljungdahl, 2017-09-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -317,7 +317,7 @@ dofire(const struct nh_cmd_arg *arg)
             ammo_and_launcher(uquiver, uswapwep) &&
             (!uwep || !uwep->cursed || !uwep->bknown) &&
             (!uswapwep->cursed || !uswapwep->bknown)) {
-            if (uwep->cursed) {
+            if (uwep && uwep->cursed) {
                 weldmsg(msgc_cancelled1, uwep);
                 return 1;
             }
