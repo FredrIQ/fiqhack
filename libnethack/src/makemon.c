a66277211892ac81085ca70304ee849564c03c4b
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-02 11:03:32 +0200

    Fix a misleading comment

--- libnethack/src/makemon.c
+++ libnethack/src/makemon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2016-02-17 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -832,7 +832,7 @@ propagate(int mndx, boolean tally, boolean ghostly)
 
 /*
  * called with [x,y] = coordinates;
- *      [0,0] means anyplace
+ *      [COLNO,ROWNO] means anyplace
  *      [u.ux,u.uy] means: near player (if !in_mklev)
  *
  *      In case we make a monster group, only return the one at [x,y].
