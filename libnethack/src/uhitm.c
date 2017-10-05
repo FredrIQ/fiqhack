e99a71cddf349c5323240b72b5a7c475b542e1cb
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-05 12:59:42 +0200

    Future-proof Cleaver logic in case of passive weapon-losing attacks
    From NetHack 3.6.1 commit 856034d58504da93cd82ee5bd4c8b06ccb641376

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-04 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-05 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -281,7 +281,7 @@ hitum(struct monst *mon, int tmp, const struct attack *uattk, schar dx,
 
         int count = 3;
         boolean result;
-        while (count--) {
+        while (count-- && uwep) {
             if (!i)
                 i = 7;
             else
