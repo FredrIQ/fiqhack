b295d9331b6a77a82a08790bbd776af56f666fc3
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-06 15:04:06 +0200

    Make nasty weapon suicides work properly

--- libnethack/src/mhitm.c
+++ libnethack/src/mhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-09-25 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -434,7 +434,7 @@ mattackm(struct monst *magr, struct monst *mdef)
                     magr->mhp -= rnd(6);
                     if (magr->mhp <= 0) {
                         mondied(magr);
-                        return 1;
+                        return MM_AGR_DIED;
                     }
                 }
 
