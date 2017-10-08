7e59840f5e3fb71c0a07e1594101e7b0c65e94d2
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-08 23:41:25 +0200

    Different message for monsters zapping discharged wands

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-06 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1967,11 +1967,14 @@ zappable(struct monst *mon, struct obj *wand)
     boolean you = (mon == &youmonst);
     boolean vis = canseemon(mon);
     if (wand->spe < 0 || (wand->spe == 0 && rn2(121))) {
-        if (you || vis) {
-            pline(you ? msgc_failrandom : msgc_monneutral,
+        if (you)
+            pline(msgc_failrandom,
                   "You feel an absence of magical power.");
+        else
+            pline(msgc_monneutral, "Nothing happens.");
+        if (you || vis)
             wand->known = 1; /* we know the :0 */
-        }
+
         if (!you)
             wand->mknown = 1; /* monster learns charge count */
         return 0;
