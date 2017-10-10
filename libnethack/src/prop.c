0f35d89e66bb18d21221e321c7dadd7b6ae48abb
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-10 15:39:00 +0200

    If a property times out and you get a message, interrupt

--- libnethack/src/prop.c
+++ libnethack/src/prop.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-09 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-10 */
 /* Copyright (c) 1989 Mike Threepoint                             */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* Copyright (c) 2014 Alex Smith                                  */
@@ -2142,6 +2142,11 @@ update_property(struct monst *mon, enum youprop prop,
         impossible("Unknown property: %u", prop);
         break;
     }
+
+    /* If a property timed out, interrupt the player. */
+    if (you && lost && slot == os_dectimeout && effect)
+        action_interrupted();
+
     return effect;
 }
 
