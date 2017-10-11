ed89e8f1fbf60d5278687b8efb7857b4b7995efa
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-11 17:47:21 +0200

    Fix a memory leak introduced after invent purge

--- libnethack/src/save.c
+++ libnethack/src/save.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-09 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1007,6 +1007,7 @@ freedynamicdata(void)
 
     /* game-state data */
     free_monchn(migrating_mons);
+    free_objchn(youmonst.minvent);
     /* this should normally be NULL between turns, but might not be due to
        the game ending where pets can follow (e.g. ascension or dungeon escape)
        or due to panicing. */
