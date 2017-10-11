26effdfbad9116136146c7c02a58c576697936b1
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-11 13:56:08 +0200

    Free object memory data on death to avoid leaving stale memobj pointers/IDs

--- libnethack/src/bones.c
+++ libnethack/src/bones.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-09 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985,1993. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -245,6 +245,9 @@ savebones(struct obj *corpse, boolean take_items)
 
     if (take_items)
         finish_paybill();
+
+    /* Don't leave any stale object memories */
+    free_memobj();
 
     mnew(&mf, NULL);
 
