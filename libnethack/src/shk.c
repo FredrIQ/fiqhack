97a7f60c3a27d39b6e94261fec61cdf5f7a00ee1
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-12 14:08:07 +0200

    Make find_oid start by searching your inventory

--- libnethack/src/shk.c
+++ libnethack/src/shk.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-11 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1752,12 +1752,12 @@ find_oid(unsigned id)
     struct monst *mon;
     int i;
 
-    /* try searching the current level, if any */
-    if (level && (obj = find_oid_lev(level, id)))
-        return obj;
-
     /* first check various obj lists directly */
     if ((obj = o_on(id, youmonst.minvent)))
+        return obj;
+
+    /* try searching the current level, if any */
+    if (level && (obj = find_oid_lev(level, id)))
         return obj;
 
     /* not found yet; check inventory for members of various monst lists */
