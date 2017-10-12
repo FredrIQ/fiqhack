ce5dac813c115830ee53f9e44baed78acffd7a50
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-12 13:35:14 +0200

    Make splitobj reset mem_obj

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-11 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -423,6 +423,7 @@ splitobj(struct obj *obj, long num)
     otmp->timed = 0;    /* not timed, yet */
     otmp->lamplit = 0;  /* ditto */
     otmp->owornmask = 0L;       /* new object isn't worn */
+    otmp->mem_obj = NULL;
     obj->quan -= num;
     obj->owt = weight(obj);
     otmp->quan = num;
