9ca64911370b533d116c40ab1459e0c61b8d34d0
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-15 00:43:04 +0200

    Removed code made redundant by recent altar tweaks

--- libnethack/src/pager.c
+++ libnethack/src/pager.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -152,15 +152,6 @@ describe_bg(int x, int y, int bg, char *buf)
     }
 
     switch (bg) {
-    case S_altar:
-        if (!In_endgame(&u.uz))
-            sprintf(buf, "%s altar",
-                    align_str(Amask2align
-                              (level->locations[x][y].flags & AM_MASK)));
-        else
-            sprintf(buf, "aligned altar");
-        break;
-
     case S_ndoor:
         if (is_drawbridge_wall(x, y) >= 0)
             strcpy(buf, "open drawbridge portcullis");
