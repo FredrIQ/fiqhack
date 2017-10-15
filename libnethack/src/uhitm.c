f6bcbbcb860490722c5e8e0ccc38bc39888c6fc9
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-15 16:12:50 +0200

    Still apply nasty damage on a miss

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1930,6 +1930,13 @@ missum(struct monst *mdef, const struct attack *mattk)
               "You miss %s.", mon_nam(mdef));
     else
         pline(combat_msgc(&youmonst, mdef, cr_miss), "You miss it.");
+    if (mattk->aatyp == AT_WEAP && uwep &&
+        (obj_properties(uwep) & opm_nasty)) {
+        pline(combat_msgc(NULL, &youmonst, cr_hit),
+              "The nasty weapon hurts you!");
+        learn_oprop(uwep, opm_nasty);
+        losehp(rnd(6), killer_msg(DIED, "a nasty weapon"));
+    }
     if (!mdef->msleeping && mdef->mcanmove)
         wakeup(mdef, FALSE);
 }
