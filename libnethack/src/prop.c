9840b07b5727a8a981a887702cae60d6b18be1d4
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-15 17:24:44 +0200

    Don't pause multiturn actions when clairvoyance procs

--- libnethack/src/prop.c
+++ libnethack/src/prop.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-13 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
 /* Copyright (c) 1989 Mike Threepoint                             */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* Copyright (c) 2014 Alex Smith                                  */
@@ -2145,7 +2145,7 @@ update_property(struct monst *mon, enum youprop prop,
     }
 
     /* If a property timed out, interrupt the player. */
-    if (you && lost && slot == os_dectimeout && effect)
+    if (you && lost && slot == os_dectimeout && effect && prop != CLAIRVOYANT)
         action_interrupted();
 
     return effect;
