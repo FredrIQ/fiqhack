3e16ff475f76316ca871fb95eb2c721f526e3072
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-15 22:36:11 +0200

    With removal of poison instadeaths, pits don't have unspeakable horrors.

--- libnethack/src/do.c
+++ libnethack/src/do.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -843,7 +843,8 @@ dodown(const struct nh_cmd_arg *arg, boolean autodig_ok)
               is_animal(u.ustuck->data) ? "swallowed" : "engulfed");
         return 1;
     }
-    if (on_level(&valley_level, &u.uz) && !u.uevent.gehennom_entered) {
+    if ((!trap || trap->ttyp == PIT || trap->ttyp == SPIKED_PIT) &&
+        on_level(&valley_level, &u.uz) && !u.uevent.gehennom_entered) {
         pline_implied(msgc_branchchange,
                       "You are standing at the gate to Gehennom.");
         pline_implied(msgc_branchchange,
