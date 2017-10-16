9197f0d6a30bae789f88352b60a5b84b8e3a8207
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-16 04:08:55 +0200

    When successfully writing a scroll or spell, formally ID it

--- libnethack/src/write.c
+++ libnethack/src/write.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2015-11-13 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-16 */
 /* NetHack may be freely redistributed.  See license for details. */
 
 #include "hack.h"
@@ -234,6 +234,8 @@ found:
     useup(paper);
 
     /* success */
+    makeknown(new_obj->otyp);
+
     if (new_obj->oclass == SPBOOK_CLASS) {
         /* acknowledge the change in the object's description... */
         pline(msgc_actionok, "The spellbook warps strangely, then turns %s.",
