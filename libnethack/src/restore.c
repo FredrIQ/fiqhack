cb360315e2a8459c3fdf00aecab5991aca51e69b
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-16 04:22:37 +0200

    Better error for object memory link errors

--- libnethack/src/restore.c
+++ libnethack/src/restore.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-16 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -289,7 +289,8 @@ restobjchn(struct memfile *mf, struct level *lev, boolean ghostly,
             if (!otmp->mem_obj)
                 otmp->mem_obj = find_oid(otmp->mem_o_id);
             if (!otmp->mem_obj)
-                impossible("Object memory link failed.");
+                impossible("Object memory link failed for: %s",
+                           killer_xname(otmp));
             else if (otmp->mem_obj->mem_obj &&
                      otmp->mem_obj->mem_obj != otmp) {
                 impossible("Duplicate object memory for %s.",
