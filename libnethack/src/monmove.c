650e4ae4345a13cada75ab9b23e72303c3244762
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-16 04:38:27 +0200

    Peacefuls can't displace each other anymore

--- libnethack/src/monmove.c
+++ libnethack/src/monmove.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-16 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -923,7 +923,8 @@ not_special:
         flag |= (ALLOW_SANCT | ALLOW_SSM);
     else
         flag |= ALLOW_MUXY;
-    flag |= ALLOW_PEACEFUL;
+    if (!mtmp->mpeaceful || mtmp->mtame)
+        flag |= ALLOW_PEACEFUL;
     if (pm_isminion(ptr) || is_rider(ptr) || is_mplayer(ptr))
         flag |= ALLOW_SANCT;
     /* unicorn may not be able to avoid hero on a noteleport level */
