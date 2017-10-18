c04f735d8bfe10bfb03b876c67872e92d502e5f2
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-18 02:25:07 +0200

    Check for existing mx_epri in histemple_at

--- libnethack/src/priest.c
+++ libnethack/src/priest.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-18 */
 /* Copyright (c) Izchak Miller, Steve Linhart, 1989.              */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -125,7 +125,8 @@ static boolean
 histemple_at(struct monst *priest, xchar x, xchar y)
 {
     return ((boolean)
-            ((mx_epri(priest)->shroom == *in_rooms(level, x, y, TEMPLE)) &&
+            (mx_epri(priest) &&
+             mx_epri(priest)->shroom == *in_rooms(level, x, y, TEMPLE) &&
              on_level(&(mx_epri(priest)->shrlevel), &u.uz)));
 }
 
