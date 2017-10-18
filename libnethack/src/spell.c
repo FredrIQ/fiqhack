65988054be13e9cf7dec5fb680e1b2e5836c29b4
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-18 02:24:43 +0200

    Use useup when finished reading a spellbook

--- libnethack/src/spell.c
+++ libnethack/src/spell.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-17 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-18 */
 /* Copyright (c) M. Stephenson 1988                               */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -474,10 +474,8 @@ learn(void)
         pline(msgc_actionok, "The spellbook disappears.");
         if (!u.utracked[tos_book])
             impossible("Spellbook already gone?");
-        else {
-            obj_extract_self(u.utracked[tos_book]);
-            obfree(u.utracked[tos_book], NULL);
-        }
+        else
+            useup(u.utracked[tos_book]);
     }
 
     return 0;
