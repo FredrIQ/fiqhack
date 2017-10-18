85434b912bc4b4b8f49216d9668ca180640908e3
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-18 03:14:37 +0200

    Handle player AT_AREA correctly

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-15 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-18 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2112,6 +2112,7 @@ hmonas(struct monst *mon, int tmp, schar dx, schar dy)
 
         case AT_NONE:
         case AT_BOOM:
+        case AT_AREA:
             continue;
             /* Not break--avoid passive attacks from enemy */
 
