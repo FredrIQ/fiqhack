db3aeb646b28614889522808bee24c43af837bec
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-19 16:22:24 +0200

    Something crashed this way, so add this for the future

--- libnethack/src/trap.c
+++ libnethack/src/trap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-16 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-19 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1755,11 +1755,13 @@ isclearpath(struct level *lev, coord * cc, int distance, schar dx, schar dy)
     return TRUE;
 }
 
-
 int
 mintrap(struct monst *mtmp)
 {
     struct level *lev = mtmp->dlevel;
+    if (!mtmp->dlevel)
+        panic("mintrap: dlevel is NULL?");
+
     struct trap *trap = t_at(lev, mtmp->mx, mtmp->my);
     boolean trapkilled = FALSE;
     const struct permonst *mptr = mtmp->data;
