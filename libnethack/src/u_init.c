fa9612653978804671b4bd9b45ac34f3ef6750b6
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-22 15:02:36 +0200

    Let Wizards always read their starting books

--- libnethack/src/u_init.c
+++ libnethack/src/u_init.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2017-06-29 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-22 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -201,8 +201,8 @@ static const struct trobj Wizard[] = {
     {WAN_STRIKING, UNDEF_SPE, WAND_CLASS, 1, UNDEF_BLESS},
     {SPE_FORCE_BOLT, 0, SPBOOK_CLASS, 1, 1},
     {SPE_MAGIC_MISSILE, 0, SPBOOK_CLASS, 1, 1},
-    {UNDEF_TYP, UNDEF_SPE, SPBOOK_CLASS, 1, UNDEF_BLESS},
-    {UNDEF_TYP, UNDEF_SPE, SPBOOK_CLASS, 1, UNDEF_BLESS},
+    {UNDEF_TYP, UNDEF_SPE, SPBOOK_CLASS, 1, 1},
+    {UNDEF_TYP, UNDEF_SPE, SPBOOK_CLASS, 1, 1},
     {BLINDFOLD, 0, TOOL_CLASS, 1, 0},
     {0, 0, 0, 0, 0}
 };
