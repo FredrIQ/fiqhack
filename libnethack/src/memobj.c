19112878db91778c2c96d472951247da0523885f
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-26 22:39:26 +0200

    Fix issues with inventory object memories

--- libnethack/src/memobj.c
+++ libnethack/src/memobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-16 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-26 */
 /* Copyright (c) Fredrik Ljungdahl, 2017. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -242,7 +242,7 @@ update_obj_memories(struct level *lev)
     /* First, mark memories as lost, or free them. */
     struct obj *obj, *memobj, *next;
     for (memobj = youmonst.meminvent; memobj; memobj = next) {
-        next = memobj->nexthere;
+        next = memobj->nobj;
         obj = memobj->mem_obj;
         if (!obj) {
             /* Apparently the object disappeared, deallocate it. */
