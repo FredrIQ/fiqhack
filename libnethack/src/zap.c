1e13979d4470587b300eb6e95e52845403185ffe
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-28 21:18:48 +0200

    Fix a bug with death rays

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-26 */
+/* Last modified by Fredrik Ljungdahl, 2017-10-28 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3228,9 +3228,11 @@ zap_hit_mon(struct monst *magr, struct monst *mdef, int type,
                                   "%s unaffected.", M_verbs(mdef, "are"));
                     }
                     break;
-                } else
+                } else {
                     mlosexp(magr, mdef,
                             "drained by a wand of death", FALSE);
+                    return;
+                }
             } else if (selfzap && (you || oseen))
                 pline(combat_msgc(magr, mdef, cr_kill),
                       "%s %sself with pure energy!", M_verbs(mdef, "irradiate"),
@@ -3275,6 +3277,7 @@ zap_hit_mon(struct monst *magr, struct monst *mdef, int type,
             if ((otmp = which_armor(mdef, os_armc)) &&
                 !item_provides_extrinsic(otmp, DISINT_RES, &dummy))
                 disint_arm(mdef, otmp);
+
             if ((otmp = which_armor(mdef, os_armu)) &&
                 !item_provides_extrinsic(otmp, DISINT_RES, &dummy))
                 disint_arm(mdef, otmp);
