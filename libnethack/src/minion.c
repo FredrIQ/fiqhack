c2fc8c74c2c884496d83b65f000d74ac8f373824
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-02 20:02:17 +0100

    Fix crash if Wizard of Yendor does the "summon nasties" harassment

--- libnethack/src/minion.c
+++ libnethack/src/minion.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-28 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -87,10 +87,12 @@ msummon(struct monst *mon, const d_level *dlev)
             /* alignment should match the summoner */
             mtmp->maligntyp = atyp;
             /* peaceful state should match too */
-            if (mon->mpeaceful)
-                msethostility(mtmp, FALSE, TRUE);
-            if (mon->mtame)
-                tamedog(mtmp, NULL);
+            if (mon) {
+                if (mon->mpeaceful)
+                    msethostility(mtmp, FALSE, TRUE);
+                if (mon->mtame)
+                    tamedog(mtmp, NULL);
+            }
         }
         cnt--;
     }
