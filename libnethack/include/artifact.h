0fa45c88b7ef1637fe5efcf5c5f3a5a822a323f2
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-02 17:31:17 +0100

    Make Dragonbane the first sacrifice gift for Knights

--- libnethack/include/artifact.h
+++ libnethack/include/artifact.h
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2014-04-05 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -81,4 +81,3 @@ enum artigen_type {
 # define SELF_UNCURSE   (LAST_PROP+10)
 
 #endif /* ARTIFACT_H */
-
--- libnethack/include/artilist.h
+++ libnethack/include/artilist.h
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-04 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -138,7 +138,7 @@ static const struct artifact const_artilist[] = {
 
     A("Dragonbane", LANCE,
       (SPFX_RESTR | SPFX_REFLECT | SPFX_DCLAS), 0, S_DRAGON,
-      PHYS(5, 20), NO_DFNS, NO_CARY, 0, A_NONE, NON_PM, NON_PM, 500L),
+      PHYS(5, 20), NO_DFNS, NO_CARY, 0, A_LAWFUL, PM_KNIGHT, NON_PM, 500L),
 
     A("Demonbane", LONG_SWORD,
       (SPFX_RESTR | SPFX_DFLAG2), 0, M2_DEMON,
