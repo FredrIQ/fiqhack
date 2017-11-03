3aa25f6ed6631cf6df00bba298a86637849551ea
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-03 10:06:23 +0100

    Give nemeses a copy of How To Pickup Artifacts by Rodney
    Tested and fixed

--- libnethack/src/wizard.c
+++ libnethack/src/wizard.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-01 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-03 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -198,7 +198,7 @@ on_ground(short otyp, xchar x, xchar y, const struct monst *mtmp)
 {
     struct obj *otmp;
 
-    if (otyp) {
+    if (otyp || x == COLNO) {
         for (otmp = level->objlist; otmp; otmp = otmp->nobj)
             if (otyp) {
                 if (otmp->otyp == otyp)
