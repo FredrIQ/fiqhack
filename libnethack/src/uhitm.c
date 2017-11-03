1cbba22b33058b0c951843e2359a8fca537d723d
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-03 20:07:00 +0100

    Player gremlins are also not restricted by time

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-03 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1405,7 +1405,7 @@ damageum(struct monst *mdef, const struct attack *mattk)
         tmp = 0;
         break;
     case AD_CURS:
-        if (night() && !rn2(10) && !cancelled(&youmonst)) {
+        if (!rn2(10) && !cancelled(&youmonst)) {
             if (mdef->data == &mons[PM_CLAY_GOLEM]) {
                 if (!blind(&youmonst))
                     pline(combat_msgc(&youmonst, mdef, cr_kill),
