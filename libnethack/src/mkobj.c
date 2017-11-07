051fc60eee2f13fb546e5f913cb0690c6eada258
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-07 09:46:35 +0100

    Correct save handling for eggs laid by you

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-06 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-07 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1893,6 +1893,8 @@ restore_obj(struct memfile *mf)
             base_spe = OPM_SPINACH;
         else if (otmp->otyp == TIN && otmp->spe == -1)
             base_spe = OPM_HOMEMADE;
+        else if (otmp->otyp == EGG && otmp->spe == 1)
+            base_spe = OPM_YOULAID;
         otmp->spe = base_spe;
 
         /* Gender fixup */
