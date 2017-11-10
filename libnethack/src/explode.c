d1e9c636fb65ff1307034ae5798d07f0eac010bf
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-10 12:55:48 +0100

    Skilled+ death fields can't be resisted

--- libnethack/src/explode.c
+++ libnethack/src/explode.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-30 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-10 */
 /* Copyright (C) 1990 by Ken Arromdee                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -356,7 +356,8 @@ explode(int x, int y, int type, /* the same as in zap.c */
                 } else {
                     int mdam = dam;
 
-                    if (resist(NULL, mtmp, olet, FALSE, 0)) {
+                    if ((adtyp != AD_DISN || !raylevel) &&
+                        resist(NULL, mtmp, olet, FALSE, 0)) {
                         /* TODO: again, message channel is a guess */
                         if (cansee(i + x - 1, j + y - 1))
                             pline(msgc_monneutral,
