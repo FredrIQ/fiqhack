b1d57774d7ca2e25dff511b47737be623e08915c
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-10 15:16:53 +0100

    Fix egg bashmsg

--- libnethack/src/uhitm.c
+++ libnethack/src/uhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-07 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-10 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -618,7 +618,7 @@ hmon_hitmon(struct monst *mon, struct obj *obj, int thrown,
                         /* egg is always either used up or transformed, so next
                            hand-to-hand attack should yield a "bashing" mesg */
                         if (obj == uwep)
-                            u.bashmsg = FALSE;
+                            u.bashmsg = TRUE;
                         if ((obj->spe & OPM_YOULAID) &&
                             obj->corpsenm >= LOW_PM) {
                             if (obj->quan < 5)
