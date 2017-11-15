0abfe7c55d35ce11b2f2809719ace5603d307a02
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-15 19:13:49 +0100

    Put a game that ended unexpectedly in recoverquit on public servers

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-08 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -376,9 +376,14 @@ nh_play_game(int fd, enum nh_followmode followmode)
         return ERR_BAD_FILE;
     case LS_DONE:
         if (followmode != FM_REPLAY && followmode != FM_RECOVERQUIT) {
+#ifdef PUBLIC_SERVER
+            /* assume only one process at most, so this is safe */
+            followmode = FM_RECOVERQUIT;
+#else
             replay_forced = TRUE;
             followmode = FM_REPLAY; /* force into replay mode */
             file_done = TRUE;
+#endif
         }
         break;
     case LS_CRASHED:
