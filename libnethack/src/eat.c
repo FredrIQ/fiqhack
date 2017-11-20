1ece4d160397a71ea4933224c71c8bd8941b7c57
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-20 18:03:38 +0100

    Fix stalkers not giving see invisible if eaten while invisible

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-19 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-20 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -608,6 +608,8 @@ cpostfx(struct monst *mon, int pm)
             mon->mhp = mon->mhpmax;
         break;
     case PM_STALKER:
+        if (invisible(mon))
+            set_property(mon, SEE_INVIS, 0, FALSE);
         set_property(mon, INVIS, invisible(mon) ? 0 : rn1(100, 50), FALSE);
         /* fall into next case */
     case PM_GIANT_BAT:
