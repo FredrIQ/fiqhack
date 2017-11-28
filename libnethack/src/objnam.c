6dbbb071c94358f8870a6c8bcca7169f48f94b54
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-28 12:12:40 +0100

    Properties that give fire/cold/shock immunity are now called as such

--- libnethack/src/objnam.c
+++ libnethack/src/objnam.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-07 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-28 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -128,10 +128,10 @@ static const struct opropdesc prop_desc[] = {
     {opm_warn, WARNING, NULL, "warn", "warning", NULL},
     {opm_aggravate, AGGRAVATE_MONSTER, NULL, "aggravates",
      "aggravate monster", NULL},
-    {opm_fire, FIRE_RES, NULL, NULL, "fire", "fire resistance"},
-    {opm_frost, COLD_RES, NULL, NULL, "frost", "cold resistance"},
+    {opm_fire, FIRE_RES, NULL, NULL, "fire", "fire immunity"},
+    {opm_frost, COLD_RES, NULL, NULL, "frost", "cold immunity"},
     {opm_shock, SHOCK_RES, NULL, "shock", "lightning",
-     "shock resistance"},
+     "shock immunity"},
     {opm_drain, DRAIN_RES, "thirsty", "drain", NULL,
      "drain resistance"},
     {opm_speed, FAST, NULL, NULL, "speed", NULL},
