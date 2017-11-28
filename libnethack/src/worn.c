6ae0ab966c7c85ed2039ad3c6df3c3f5389f61ce
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-11-28 02:20:28 +0100

    Sunsword blocks blindness, like Eyes of the Overworld

--- libnethack/src/worn.c
+++ libnethack/src/worn.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-19 */
+/* Last modified by Fredrik Ljungdahl, 2017-11-28 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -14,7 +14,8 @@ static unsigned do_equip(struct monst *, struct obj *, boolean, boolean);
 /* TODO: maybe make this into a real function */
 #define w_blocks(o,m) \
     ((o->otyp == MUMMY_WRAPPING && ((m) & W_MASK(os_armc))) ? INVIS :   \
-     (o->oartifact == ART_EYES_OF_THE_OVERWORLD &&                      \
+     ((o->oartifact == ART_EYES_OF_THE_OVERWORLD ||                     \
+       o->oartifact == ART_SUNSWORD) &&                                 \
       ((m) & W_MASK(os_tool))) ? BLINDED :                              \
      (o->otyp == CORNUTHAUM && ((m) & W_MASK(os_armh)) &&               \
       !Role_if (PM_WIZARD)) ? CLAIRVOYANT : 0)
