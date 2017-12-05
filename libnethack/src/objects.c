e89a9c125f9ccba91b6a9917d50f382cf4665186
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-05 07:35:18 +0100

    Fix SoR weight

--- libnethack/src/objects.c
+++ libnethack/src/objects.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-19 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-05 */
 /* Copyright (c) Mike Threepoint, 1989.                           */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -448,7 +448,7 @@ const struct objclass const_objects[] = {
     SHIELD("dwarvish roundshield", "large round shield",
            0, 0, 0, 0, 4, 0, 180, 10, 7, 0, IRON, HI_METAL),
     SHIELD("shield of reflection", "polished silver shield",
-           0, 1, 0, REFLECTING, 3, 0, 200, 50, 8, 0, SILVER, HI_SILVER),
+           0, 1, 0, REFLECTING, 3, 0, 100, 50, 8, 0, SILVER, HI_SILVER),
 
 /* gloves */
 /* these have their color but not material shuffled, so the IRON must stay
