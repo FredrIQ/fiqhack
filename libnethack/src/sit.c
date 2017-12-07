386bd445f037d069abb135e973e213543403d2a2
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-07 13:26:30 +0100

    Fix erroneous message if Magicbane helps you resist a cursing

--- libnethack/src/sit.c
+++ libnethack/src/sit.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-19 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-07 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -353,7 +353,6 @@ rndcurse(struct monst *mtmp, struct monst *magr)
     int nobj = 0;
     int cnt, onum;
     struct obj *otmp;
-    static const char mal_aura[] = "You feel a malignant aura surround %s.";
     boolean saddle;
     boolean you = (mtmp == &youmonst);
     boolean vis = canseemon(mtmp);
@@ -363,8 +362,9 @@ rndcurse(struct monst *mtmp, struct monst *magr)
 
     if ((otmp = m_mwep(mtmp)) && otmp->oartifact == ART_MAGICBANE) {
         if (you || vis)
-            pline(combat_msgc(magr, mtmp, cr_miss), mal_aura,
-                  "the magic-absorbing blade");
+            pline(combat_msgc(magr, mtmp, cr_miss),
+                  "You feel a malignant aura surround the magic-absorbing %s.",
+                  otmp->otyp == QUARTERSTAFF ? "staff" : "blade");
         cnt = rn2_on_rng(6, rng);
     }
     for (otmp = mtmp->minvent; otmp; otmp = otmp->nobj) {
