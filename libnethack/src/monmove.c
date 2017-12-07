4aa45cdef07e0bd746a6e159d97fb69914b543ae
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-07 13:46:38 +0100

    Minotaurs now respect Elbereth

--- libnethack/src/monmove.c
+++ libnethack/src/monmove.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Alex Smith, 2016-06-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-07 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -103,8 +103,7 @@ onscary(int x, int y, const struct monst *mtmp)
 {
     if (mx_eshk(mtmp) || mx_egd(mtmp) || mtmp->iswiz || blind(mtmp) ||
         mtmp->mpeaceful || mtmp->data->mlet == S_HUMAN || is_lminion(mtmp) ||
-        mtmp->data == &mons[PM_ANGEL] || is_rider(mtmp->data) ||
-        mtmp->data == &mons[PM_MINOTAUR])
+        mtmp->data == &mons[PM_ANGEL] || is_rider(mtmp->data))
         return FALSE;
 
     return (boolean) (sobj_at(SCR_SCARE_MONSTER, level, x, y)
