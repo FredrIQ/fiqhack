2d939c72b7b7550e49e08e0f199c97ba2fcbd18b
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-08 01:01:48 +0100

    Fix polyself HP too in savelife

--- libnethack/src/end.c
+++ libnethack/src/end.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-02 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -396,6 +396,7 @@ savelife(int how)
     turnstate.force_more_pending_until_done = FALSE;
     u.uswldtim = 0;
     u.uhp = u.uhpmax;
+    u.mh = u.mhmax;
     if (u.uhunger < 500) {
         u.uhunger = 500;
         newuhs(FALSE);
