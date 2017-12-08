09ab1d1ab1d3fa9aa2ef6b27b560422bf9b9874a
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-08 19:21:28 +0100

    Fix bug with expensive cameras

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-07 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2855,7 +2855,7 @@ bhit_at(struct monst *mon, struct obj *obj, int x, int y, int range)
     if (mdef) {
         if (!canseemon(mdef) && cansee(x, y))
             map_invisible(x, y);
-        bhitm(mon, mdef, obj, min(range, 1));
+        bhitm(mon, mdef, obj, max(range, 1));
         ret |= BHIT_MON;
     } else if ((mdef = vismon_at(lev, x, y)))
         ret |= BHIT_DMON;
