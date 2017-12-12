542bdeea3969e80c00d0dbcd5a1eb74476e22e0e
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-12 20:30:59 +0100

    Fix thrown food not interacting correctly with pets

--- libnethack/src/dog.c
+++ libnethack/src/dog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-11-19 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -795,7 +795,8 @@ tamedog(struct monst *mtmp, struct obj *obj)
        realtime effects means that this won't really sync anyway; this also
        calls set_malign (thus there's no need for the caller to call it after
        calling tamedog()) */
-    msethostility(mtmp, FALSE, TRUE);
+    if (!mtmp->mtame)
+        msethostility(mtmp, FALSE, TRUE);
     if (flags.moonphase == FULL_MOON && night() && rn2(6) && obj &&
         mtmp->data->mlet == S_DOG)
         return NULL;
