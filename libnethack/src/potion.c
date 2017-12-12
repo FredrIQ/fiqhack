b1ac0fd468756acbac22772eeecfa911963d36bf
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-12 08:52:34 +0100

    Fix healing bugs

--- libnethack/src/potion.c
+++ libnethack/src/potion.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-08 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1255,18 +1255,24 @@ healup(int nhp, int nxtra, boolean curesick, boolean cureblind)
     int res = 0;
     if (nhp) {
         if (Upolyd) {
-            if (u.mh < u.mhmax) {
-                u.mh += nhp;
-                if (u.mh > u.mhmax)
-                    u.mh = (u.mhmax += nxtra);
+            if (u.mh < u.mhmax)
                 res++;
+
+            u.mh += nhp;
+            if (u.mh > u.mhmax) {
+                u.mh = (u.mhmax += nxtra);
+                if (nxtra)
+                    res++;
             }
         } else {
-            if (u.uhp < u.uhpmax) {
-                u.uhp += nhp;
-                if (u.uhp > u.uhpmax)
-                    u.uhp = (u.uhpmax += nxtra);
+            if (u.uhp < u.uhpmax)
                 res++;
+
+            u.uhp += nhp;
+            if (u.uhp > u.uhpmax) {
+                u.uhp = (u.uhpmax += nxtra);
+                if (nxtra)
+                    res++;
             }
         }
     }
