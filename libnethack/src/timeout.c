97694c120ad13820f2e1cb9242115dc0021ed4c5
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-14 00:09:45 +0100

    Fix eggs always hatching tame

--- libnethack/src/timeout.c
+++ libnethack/src/timeout.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-29 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-14 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -126,7 +126,8 @@ hatch_egg(void *arg, long timeout)
     mon = mon2 = NULL;
     mnum = big_to_little(egg->corpsenm);
     /* The identity of one's father is learned, not innate */
-    yours = (egg->spe || (!u.ufemale && carried(egg) && !rn2(2)));
+    yours = ((egg->spe & OPM_YOULAID) ||
+             (!u.ufemale && carried(egg) && !rn2(2)));
     silent = (timeout != moves);        /* hatched while away */
 
     /* only can hatch when in INVENT, FLOOR, MINVENT */
