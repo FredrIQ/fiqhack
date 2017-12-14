d4e0b10da37d114dd4bc30a25fa9a3d62b6eb15b
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-15 00:06:16 +0100

    Fix monster jumping animations

--- libnethack/src/dothrow.c
+++ libnethack/src/dothrow.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-14 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -640,6 +640,7 @@ mhurtle_step(void *arg, int x, int y)
     newsym(x, y);
     set_apparxy(mon);
     mintrap(mon);
+    flush_screen();
     if (cansee(ox, oy) && cansee(x, y))
         win_delay_output();
     return TRUE;
