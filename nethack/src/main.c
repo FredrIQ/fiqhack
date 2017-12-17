564118e30f57ccbb36a20ea4ca1115e770d188dc
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-17 22:43:45 +0100

    Fix nethack4 -> fiqhack in usage help

--- nethack/src/main.c
+++ nethack/src/main.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-16 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-17 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -431,7 +431,7 @@ process_args(int argc, char *argv[])
         switch (argv[0][1]) {
         case '-':
             if (!strcmp(argv[0], "--help")) {
-                puts("Usage: nethack4 [--interface PLUGIN] [OPTIONS]");
+                puts("Usage: fiqhack [--interface PLUGIN] [OPTIONS]");
                 puts("");
                 puts("-k          connection-only mode");
                 puts("-D          start games in wizard mode");
