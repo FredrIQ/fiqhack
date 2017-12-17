84933ab1e96218aa68c85f65350eb6276f94f37f
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-18 00:01:34 +0100

    Fix segfault

--- nethack/src/status.c
+++ nethack/src/status.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-17 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-18 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -399,12 +399,11 @@ curses_update_status(struct nh_player_info *pi)
 
     if (statuswin)
         wnoutrefresh(statuswin);
-    if (oldpi.followmode != pi->followmode ||
-        (pi->followmode == FM_REPLAY && pi->moves != oldpi.moves)) {
+    if (oldpi.followmode != player.followmode) {
         draw_frame();
         wrefresh(basewin);
     }
-    oldpi = *pi;
+    oldpi = player;
 }
 
 void
