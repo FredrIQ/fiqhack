27aa17ab84a2d572704245eb11108f1223632719
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-17 23:06:31 +0100

    Only let players (not watchers/replayers) check for mail

--- libnethack/src/mail.c
+++ libnethack/src/mail.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Jonadab the Unsightly One, 2015-09-30 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-17 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -73,6 +73,9 @@ delivermail(const char *from, const char *message)
 void
 checkformail(void)
 {
+    if (program_state.followmode != FM_PLAY)
+        return;
+
     char *box, *msg;
     FILE* mb;
     char curline[102];
