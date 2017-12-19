3f1775b6b5355eed97e65d1badd8c08e5b24c6a5
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-19 15:56:15 +0100

    Fix a bug with resist checks

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-10 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-19 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3454,6 +3454,8 @@ zap_hit_mon(struct monst *magr, struct monst *mdef, int type,
             } else if (resisted == 3)
                 pline(combat_msgc(magr, mdef, cr_resist),
                       "%s a bit %s.", M_verbs(mdef, "feel"), ztyp);
+            else
+                return;
         }
     }
     if (tmp && spellcaster)
