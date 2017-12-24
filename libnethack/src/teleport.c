d21ad52f6588bf3bfde63f961e42e9a96b86e921
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-24 20:37:39 +0100

    Make level teleporters not disappear on use
    Catacombs now has regular teleporters

--- libnethack/src/teleport.c
+++ libnethack/src/teleport.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-21 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-24 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -935,8 +935,6 @@ level_tele_trap(struct trap *trap)
               "You are momentarily blinded by a flash of light.");
     else
         pline(msgc_nonmonbad, "You are momentarily disoriented.");
-    deltrap(level, trap);
-    newsym(u.ux, u.uy); /* get rid of trap symbol */
     level_tele();
 }
 
