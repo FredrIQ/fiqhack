e1f3697886e698616c76647c8e674699ecdc3425
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-27 13:23:57 +0100

    Fix memory leak in lz4 decompression (my fault)

--- libnethack/src/log.c
+++ libnethack/src/log.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-26 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-27 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -618,6 +618,7 @@ base64_decode(const char *in, char *out, int outlen)
         } else if (*in == '#') {
             /* lz4-compressed */
             int result = LZ4_decompress_safe(o, out, pos, blen);
+            free(o);
             if (result < 0) {
                 raw_printf("lz4-decompressing save failed\n");
                 error_reading_save("");
