35ec6cd6a13d226a6f346271820cff0eb3c40d5b
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-31 20:04:38 +0100

    Fix messaging in replaymode

--- libnethack/src/logreplay.c
+++ libnethack/src/logreplay.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-30 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-31 */
 /* Copyright (c) Fredrik Ljungdahl, 2017. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -147,7 +147,7 @@ replay_update_status(struct nh_player_info *unused)
 static void
 replay_print_message(int turn, enum msg_channel msgc, const char *message)
 {
-    /* TODO */
+    orig_winprocs.win_print_message(turn, msgc, message);
 }
 
 static void
