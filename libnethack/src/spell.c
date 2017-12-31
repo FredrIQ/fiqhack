f662105523cfe3daf7a752417e6ee13c9d76f2aa
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-12-31 20:05:00 +0100

    Only piss off peacefuls/tame with nasties if you used the spell

--- libnethack/src/spell.c
+++ libnethack/src/spell.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-24 */
+/* Last modified by Fredrik Ljungdahl, 2017-12-31 */
 /* Copyright (c) M. Stephenson 1988                               */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1871,7 +1871,7 @@ spelleffects(boolean atme, struct musable *m)
             pline(msgc_substitute, "You feel lonely.");
         /* summoning aggravates the target */
         struct monst *mtmp = m_at(level, cc.x, cc.y);
-        if (mtmp) {
+        if (mtmp && you) {
             if (mtmp->mtame)
                 abuse_dog(mtmp);
             else if (mtmp->mpeaceful)
