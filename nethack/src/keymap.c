4b63f7765100a2f494d48a940dffd974cbb628e2
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-05 03:45:46 +0100

    Fix: instead of aliasing drink to drink, alias it to save

--- nethack/src/keymap.c
+++ nethack/src/keymap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-04 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-05 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -301,7 +301,7 @@ handle_internal_cmd(struct nh_cmd_desc **cmd, struct nh_cmd_arg *arg,
         *cmd = find_command("mail");
     if (*cmd == find_command("drink") &&
         ui_flags.current_followmode != FM_PLAY)
-        *cmd = find_command("drink");
+        *cmd = find_command("save");
 
     int id = (*cmd)->flags & ~(CMD_UI | DIRCMD | DIRCMD_RUN | DIRCMD_GO);
     nh_bool cancel_yskip = TRUE;
