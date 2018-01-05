64dc948bab39236556b7ee284a876c74439968d3
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-05 23:07:09 +0100

    Better place for mail checking

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-02 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-05 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -666,9 +666,6 @@ you_moved(void)
     do {        /* hero can't move this turn loop */
         wtcap = near_capacity();
 
-        if (flags.servermail)
-            checkformail();
-
         do {
             /* Players have taken 1 more action than the global, monsters have
                taken 0 more actions than the global. */
@@ -704,6 +701,7 @@ you_moved(void)
             /* turn boundary handling starts here */
             /**************************************/
 
+            checkformail();
             mcalcdistress();    /* adjust monsters' trap, blind, etc */
             do_at_area(level);  /* perform AT_AREA attacks */
 
