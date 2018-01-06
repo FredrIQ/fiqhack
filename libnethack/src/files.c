2e337b5dcd9558f2bd860d66bf491a31c010fa60
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-06 01:34:44 +0100

    Fix extrainfo segfault

--- libnethack/src/files.c
+++ libnethack/src/files.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-05 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1185,7 +1185,7 @@ update_whereis(boolean playing)
         sortval += (depth(&u.uz));
     }
 
-    buf = msgprintf("%d|%c %s", sortval, buf, amulet ? 'A' : ' ');
+    buf = msgprintf("%d|%c %s", sortval, amulet ? 'A' : ' ', buf);
 
     const char *extrainfo = msgprintf(EXTRAINFO_FN, user);
 #ifdef S_IRGRP   /* the OS has per-group permissions */
