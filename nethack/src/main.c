861acdb289808034dcd41991ad7489049ea436b3
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-08 21:08:36 +0100

    add ALLOW_REPLAY define to explicitly allow replaymode on servers

--- nethack/src/main.c
+++ nethack/src/main.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-05 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-08 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -45,7 +45,7 @@ enum menuitems {
 static struct nh_menuitem mainmenu_items[] = {
     {NEWGAME, MI_NORMAL, "new game", 'n'},
     {LOAD, MI_NORMAL, "load game", 'l'},
-#ifndef PUBLIC_SERVER
+#if !defined(PUBLIC_SERVER) || defined(ALLOW_REPLAY)
     {REPLAY, MI_NORMAL, "view replay", 'v'},
 #endif
     {OPTIONS, MI_NORMAL, "set options", 'o'},
