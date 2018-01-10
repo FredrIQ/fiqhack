500f554d2cf37930e0ab5c72b32551739c18775a
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-10 19:19:23 +0100

    Blessed stethoscopes never take a turn to use

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-08 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-10 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -208,6 +208,9 @@ use_stethoscope(struct obj *obj, const struct nh_cmd_arg *arg)
         return 0;
 
     res = (moves == obj->lastused) && (flags.actions == obj->spe);
+    if (obj->blessed)
+        res = 0;
+
     obj->lastused = moves;
     obj->spe = flags.actions;
 
