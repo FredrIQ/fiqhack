be7121237c7d57927722db67a140059f607d49b9
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-10 05:48:18 +0100

    add mtense, a monster equivalent of objects' otense

--- libnethack/src/do_name.c
+++ libnethack/src/do_name.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-29 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-10 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -752,6 +752,16 @@ s_suffix(const char *s)
         return msgcat(s, "'");
     else
         return msgcat(s, "'s");
+}
+
+/* Monster version of otense. */
+const char *
+mtense(const struct monst *mon, const char *verb)
+{
+    if (mon == &youmonst)
+        return verb;
+    else
+        return vtense(NULL, verb);
 }
 
 /* Given a monster (or &youmonst) and a verb as an argument, produces "the
