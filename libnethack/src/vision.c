e177923a1a20049375c06eb8b24695c5eca984a0
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-13 04:45:30 +0100

    Plane of Water no longer blocks vision bubble-wise

--- libnethack/src/vision.c
+++ libnethack/src/vision.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-10-14 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-13 */
 /* Copyright (c) Dean Luick, with acknowledgements to Dave Cohrs, 1990. */
 /* NetHack may be freely redistributed.  See license for details.       */
 
@@ -148,8 +148,9 @@ does_block(struct level *lev, int x, int y)
          (loc->flags & (D_CLOSED | D_LOCKED | D_TRAPPED))))
         return 1;
 
-    if (loc->typ == CLOUD || loc->typ == WATER ||
-        (loc->typ == MOAT && Underwater))
+    if (loc->typ == CLOUD ||
+        (Underwater && !Is_waterlevel(&u.uz) && loc->typ != WATER &&
+         loc->typ != MOAT))
         return 1;
 
     /* Boulders block light. */
@@ -157,7 +158,8 @@ does_block(struct level *lev, int x, int y)
         return 1;
 
     /* Mimics mimicing a door or boulder block light. */
-    if ((mon = m_at(lev, x, y)) && (!invisible(mon) || see_invisible(&youmonst)) &&
+    if ((mon = m_at(lev, x, y)) &&
+        (!invisible(mon) || see_invisible(&youmonst)) &&
         ((mon->m_ap_type == M_AP_FURNITURE &&
           (mon->mappearance == S_hcdoor || mon->mappearance == S_vcdoor)) ||
          (mon->m_ap_type == M_AP_OBJECT && mon->mappearance == BOULDER)))
