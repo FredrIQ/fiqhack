a6c9166adeed4a8b2f0b4212687d18e8a936118e
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-16 08:47:30 +0100

    Fix double-space in minion names

--- libnethack/src/priest.c
+++ libnethack/src/priest.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-15 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-16 */
 /* Copyright (c) Izchak Miller, Steve Linhart, 1989.              */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -298,7 +298,7 @@ priestname(const struct monst *mon, boolean override_hallu)
             pname = msgcat(pname, "guardian ");
         if (mon->data != &mons[PM_ALIGNED_PRIEST] &&
             mon->data != &mons[PM_HIGH_PRIEST]) {
-            pname = msgcat_many(pname, what, " ", NULL);
+            pname = msgcat(pname, what);
         }
         if (mon->data != &mons[PM_ANGEL]) {
             if (isminion(mon))
