75341f89d1a40f74acbe0215802dd28bd719d263
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-21 03:53:59 +0100

    Fix a bug with object menu prompts

--- nethack/src/menu.c
+++ nethack/src/menu.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-20 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-21 */
 /* Copyright (c) Daniel Thaler, 2011 */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1058,9 +1058,10 @@ curses_display_objects(
                 }
             }
 
-            if (grouphits)
+            if (grouphits) {
                 mdat->selcount = -1;
-            continue;
+                continue;
+            }
         }
 
         if (!scroll_using_key(&(mdat->s), key, &done))
