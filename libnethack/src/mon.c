be5245e1aa3643bb4d6aba982bf15c655891ea5f
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-21 14:29:16 +0100

    Remove stray debug line

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-20 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-21 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1368,7 +1368,6 @@ mtamedog(struct monst *master, struct monst *pet, struct obj *obj)
         sethostility(pet, !(master->mpeaceful || master->mtame), TRUE);
     }
 
-    pline(msgc_fatal, "!");
     clear_pet_loops(pet);
 
     if (tame_to(pet) == master)
