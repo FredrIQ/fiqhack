1105fd0840e7b7b919768ec6b5074e221a292b49
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-24 02:49:16 +0100

    Fix a bug that made most shields not give any spellcast penalty

--- libnethack/src/spell.c
+++ libnethack/src/spell.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-21 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-24 */
 /* Copyright (c) M. Stephenson 1988                               */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2384,8 +2384,8 @@ percent_success(const struct monst *mon, int spell)
 
     /* Wearing anything but a light shield makes it very awkward to cast a
        spell.  The penalty is not quite so bad for the player's role-specific
-       spell. */
-    if (arms && weight(arms) > (int)objects[SMALL_SHIELD].oc_weight &&
+       spell. Double worn shield weight to undo the weight halving. */
+    if (arms && (weight(arms) * 2) > (int)objects[SMALL_SHIELD].oc_weight &&
         mon->data->mlet != S_ANGEL) {
         if (you && spell == urole.spelspec) {
             chance /= 2;
