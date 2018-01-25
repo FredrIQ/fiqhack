d9820d2d4f4f7a893d864e42298b5f2ce790ca6b
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-25 04:36:51 +0100

    Fixed crysknives never revert

--- libnethack/src/do.c
+++ libnethack/src/do.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-13 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-25 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -632,9 +632,8 @@ obj_no_longer_held(struct obj *obj)
     }
     switch (obj->otyp) {
     case CRYSKNIFE:
-        /* KMH -- Fixed crysknives have only 10% chance of reverting */
         /* only changes when not held by player or monster */
-        if (!obj->oerodeproof || !rn2_on_rng(10, rng_unfix_crysknife)) {
+        if (!obj->oerodeproof) {
             obj->otyp = WORM_TOOTH;
             obj->oerodeproof = 0;
         }
