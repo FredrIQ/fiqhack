25c0cc6d90223df8e619d722a13168e61b22e784
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-02-21 18:49:42 +0100

    Bones player monsters start out asleep

--- libnethack/src/bones.c
+++ libnethack/src/bones.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-15 */
+/* Last modified by Fredrik Ljungdahl, 2018-02-21 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985,1993. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -355,6 +355,9 @@ make_bones:
         level = NULL;
         m_dowear(mtmp, TRUE);
         level = oldlev;
+
+        if (charmed)
+            mtmp->msleeping = TRUE;
     }
     if (mtmp) {
         mtmp->m_lev = (u.ulevel ? u.ulevel : 1);
