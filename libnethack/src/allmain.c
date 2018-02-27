d25aa157adbc09a15aa0005a25f290f0b5072cbe
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-02-27 11:26:44 +0100

    Only do [N turns] for helplessness for now, occupations seems to desync

--- libnethack/src/allmain.c
+++ libnethack/src/allmain.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-22 */
+/* Last modified by Fredrik Ljungdahl, 2018-02-27 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -362,9 +362,12 @@ nh_create_game(int fd, struct nh_option_desc *opts_orig)
 static void
 print_missed_turncount(void)
 {
+#if 0
+    /* BUG: causes desyncs */
     if (!flags.interrupted && flags.incomplete &&
         !((1 << flags.occupation) & ocm_farmove))
         return;
+#endif
 
     struct eyou *you = mx_eyou(&youmonst);
     if (!you)
