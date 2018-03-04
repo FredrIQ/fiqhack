f4632c10d9b09e986a927e13b01068991a5f7b30
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-04 21:56:19 +0100

    Decrease sleep time instead of waking up 10% of the time in combat

--- libnethack/src/mhitu.c
+++ libnethack/src/mhitu.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-03-03 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-04 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -636,9 +636,14 @@ mattacku(struct monst *mtmp)
         /* give player a chance of waking up before dying -kaa */
         if (sum[i] == 1) {      /* successful attack */
             if (u_helpless(hm_asleep) &&
-                turnstate.helpless_timers[hr_asleep] < moves && !rn2(10))
-                cancel_helplessness(hm_asleep,
-                                    "The combat suddenly awakens you.");
+                turnstate.helpless_timers[hr_asleep]) {
+                int awakening = rnd(5);
+                if (turnstate.helpless_timers[hr_asleep] <= awakening)
+                    cancel_helplessness(hm_asleep,
+                                        "The combat suddenly awakens you.");
+                else
+                    turnstate.helpless_timers[hr_asleep] -= awakening;
+            }
         }
         if (sum[i] == 2)
             return 1;   /* attacker dead */
