9fdbe8e9d44206a067a444386b40b28c4e49ad1e
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-06 02:09:07 +0100

    Remove hint about quivering spells from the (Q)uiver prompt

--- libnethack/src/wield.c
+++ libnethack/src/wield.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-20 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -346,9 +346,6 @@ doswapweapon(const struct nh_cmd_arg *arg)
 int
 dowieldquiver(const struct nh_cmd_arg *arg)
 {
-    pline(msgc_controlhelp,
-          "You can quiver spells by using the spellcast menu.");
-
     struct obj *newquiver = getargobj(arg, wield_objs, "quiver");
     if (!newquiver)
         return 0;
@@ -738,4 +735,3 @@ unwield_silently(struct obj *obj)
 }
 
 /*wield.c*/
-
