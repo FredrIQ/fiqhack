e39c21d12840bb12c6a66d347dc2353027110a97
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-12 10:21:32 +0100

    Don't say "X looks much better" if you can't see X

--- libnethack/src/zap.c
+++ libnethack/src/zap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-20 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -420,7 +420,7 @@ bhitm(struct monst *magr, struct monst *mdef, struct obj *otmp, int range)
                     newsym(mdef->mx, mdef->my);
                 } else
                     mimic_hit_msg(mdef, otyp);
-            } else
+            } else if (mdef == &youmonst || canseemon(mdef))
                 pline(msgc_actionok, "%s %s%s better.", Monnam(mdef),
                       mfeel(mdef),
                       otyp == SPE_EXTRA_HEALING ? " much" : "");
