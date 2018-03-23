51c9be0ceffedc686dc6e26ad1fdb157dc81fe58
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-23 12:37:27 +0100

    Better wording on corridorbranch

--- libnethack/src/options.c
+++ libnethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-01 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-23 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -160,7 +160,7 @@ static const struct nh_option_desc const_options[] = {
      "unlock known locked doors when walking into them",
      nh_birth_ingame, OPTTYPE_BOOL, {.b = TRUE}},
     {"corridorbranch", "Commands and Confirmations",
-     "branching corridors do not stop farmove",
+     "if true, don't stop on branching corridors",
      nh_birth_ingame, OPTTYPE_BOOL, {.b = TRUE}},
     {"disclose", "Endgame Sequence",
      "whether to disclose information at end of game",
