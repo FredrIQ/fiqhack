37e8c31cf9e73628a6bb917bc740db0406bcc6e8
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-26 13:17:24 +0200

    RIngs of carrying can be eaten for an effect now

--- libnethack/src/eat.c
+++ libnethack/src/eat.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-02-21 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-26 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1446,6 +1446,10 @@ eataccessory(struct monst *mon, struct obj *otmp)
             accessory_has_effect(mon, otmp);
             set_property(mon, PROTECTION, 0, FALSE);
             mon->mac += otmp->spe;
+            break;
+        case RIN_CARRYING:
+            accessory_has_effect(mon, otmp);
+            mon->carryinc += otmp->spe;
             break;
         case AMULET_OF_CHANGE:
             accessory_has_effect(mon, otmp);
