d8b764f733d71a9bc6710cc9898450d73f56e918
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-03-30 21:57:15 +0200

    Fix segfault with shopkeeper reviving

--- libnethack/src/shk.c
+++ libnethack/src/shk.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-19 */
+/* Last modified by Fredrik Ljungdahl, 2018-03-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -657,6 +657,8 @@ shopper_financial_report(void)
 int
 inhishop(const struct monst *mtmp)
 {
+    if (!mx_eshk(mtmp))
+        return FALSE;
     return (strchr
             (in_rooms(mtmp->dlevel, mtmp->mx, mtmp->my, SHOPBASE),
              mx_eshk(mtmp)->shoproom) &&
