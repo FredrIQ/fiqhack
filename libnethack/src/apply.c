f24b0051b0ba05c5ee84853d9af08da6c1f3eab4
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-01 16:48:26 +0200

    Fix issues with polearm targeting

--- libnethack/src/apply.c
+++ libnethack/src/apply.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-02-21 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-01 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2686,7 +2686,7 @@ find_polearm_target_recurse(struct obj *obj, int minr, int maxr, coord *cc,
                 continue;
 
             mon = vismon_at(level, x, y);
-            if (mon && !mon->mpeaceful) {
+            if (mon && !mon->mpeaceful && canspotmon(mon)) {
                 cc->x = x;
                 cc->y = y;
                 return TRUE;
