67710497ef807f617487fc5007811ca11008e5dd
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-01 17:36:20 +0200

    Don't allow rings of carrying and carrying

--- libnethack/src/mkobj.c
+++ libnethack/src/mkobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2017-12-25 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-01 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -353,6 +353,9 @@ obj_properties(const struct obj *obj)
 
     if (obj->otyp == HELM_OF_BRILLIANCE)
         props &= ~opm_brilliance;
+
+    if (obj->otyp == RIN_CARRYING)
+        props &= ~opm_carrying;
 
     /* Only leather objects, avoid redundancy */
     if (objects[obj->otyp].oc_material != CLOTH ||
