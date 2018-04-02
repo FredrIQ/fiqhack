61d9ff9e9985237dc7ae4453f2ac50a35de39f50
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-02 23:11:37 +0200

    Remove the easy mode "advertisement"

--- libnethack/src/cmd.c
+++ libnethack/src/cmd.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-01 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -827,8 +827,6 @@ dowelcome(const struct nh_cmd_arg *arg)
 
     if (flags.easy)
         pline(msgc_intro, "You are in a mostly safe place, don't worry.");
-    else
-        pline(msgc_intro, "Game too hard? Try the new easy mode.");
 
     if (discover)
         pline(msgc_intro, "You are in non-scoring discovery mode.");
