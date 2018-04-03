4ebf50922024cebfb71f1755c7188e667feeaa7c
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-04 00:35:31 +0200

    Try harder to avoid peacefuls hiding

--- libnethack/src/monmove.c
+++ libnethack/src/monmove.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-03-22 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-04 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1431,6 +1431,9 @@ postmov:
                     OBJ_AT(mtmp->mx, mtmp->my) :
                     (is_pool(level, mtmp->mx, mtmp->my) &&
                      !Is_waterlevel(&u.uz));
+            if (mtmp->mpeaceful)
+                mtmp->mundetected = FALSE;
+
             newsym(mtmp->mx, mtmp->my);
         }
         if (mx_eshk(mtmp)) {
