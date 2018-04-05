826a06908b739093459ed2126befb12fb392e2f4
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-05 15:52:09 +0200

    Swimmers have doubled speed in the Plane of Water

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-03 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-05 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -564,6 +564,9 @@ mcalcmove(struct monst *mon)
         mon = u.usteed;
 
     int mmove = mon->data->mmove;
+
+    if (Is_waterlevel(m_mz(mon)) && swims(mon))
+        mmove *= 2;
 
     /* Note: slow's `+ 1' prevents slowed speed 1 getting reduced to 0;
        fast's `+ 2' prevents hasted speed 1 from becoming a no-op; both
