61ef2fc191c5d9c36193fab2135f1f5ace0f5193
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-06 12:11:32 +0200

    Summoned monsters don't get any inventory

--- libnethack/src/makemon.c
+++ libnethack/src/makemon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-04 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1197,7 +1197,7 @@ create_critters(int cnt, const struct permonst *mptr, int x, int y)
             x = c.x, y = c.y;
 
         mon = makemon(mptr, level, x, y,
-                      MM_CREATEMONSTER |
+                      MM_CREATEMONSTER | NO_MINVENT |
                       (you ? MM_CMONSTER_U : MM_CMONSTER_M) |
                       (you ? 0 : MM_ADJACENTOK));
         if (mon && cansuspectmon(mon))
--- libnethack/src/wizard.c
+++ libnethack/src/wizard.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-03-28 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-06 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -879,11 +879,11 @@ nasty(struct monst *mcast, coord bypos)
                         &mons[makeindex]))
                 continue;
             mtmp = makemon(&mons[makeindex], level, bypos.x, bypos.y,
-                           (peace == 3) ? MM_EDOG : NO_MM_FLAGS);
+                           ((peace == 3) ? MM_EDOG : 0) | NO_MINVENT);
             if (!mtmp) {
                 /* probably genocided, try a random monster */
                 mtmp = makemon(NULL, level, bypos.x, bypos.y,
-                               (peace == 3) ? MM_EDOG : NO_MM_FLAGS);
+                               ((peace == 3) ? MM_EDOG : 0) | NO_MINVENT);
                 if (!mtmp) /* failed again? */
                     continue;
             }
