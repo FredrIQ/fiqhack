90b76f82b2cdb121e2ca96cae15567a2467787e0
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-15 16:52:38 +0200

    Allow '.' as an alias to ',' for getobj (SLASH'EM muscle memory)

--- libnethack/src/invent.c
+++ libnethack/src/invent.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-01 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1042,6 +1042,9 @@ getobj(const char *let, const char *word, boolean isarg)
         }
         if (cnt == -1)
             cnt = 0;
+
+        if (ilet == '.')
+            ilet = ','; /* alias from SLASH'EM muscle memory */
 
         if (strchr(quitchars, ilet)) {
             pline(msgc_cancelled, "Never mind.");
