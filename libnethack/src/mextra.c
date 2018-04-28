33b68b21fd4b0f9e75f9935605ef796da520b046
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-04-28 15:59:43 +0200

    Fix issues with shops

--- libnethack/src/mextra.c
+++ libnethack/src/mextra.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-12 */
+/* Last modified by Fredrik Ljungdahl, 2018-04-28 */
 /* Copyright (c) Fredrik Ljungdahl, 2015. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -456,6 +456,8 @@ save_mextra(struct memfile *mf, const struct monst *mon)
         mwrite32(mf, mx->eshk->debit);
         mwrite32(mf, mx->eshk->loan);
         mwrite16(mf, mx->eshk->shoptype);
+        if (mx->eshk->shoptype < SHOPBASE)
+            mx->eshk->shoptype += 20;
         mwrite16(mf, mx->eshk->billct);
         mwrite16(mf, mx->eshk->visitct);
         mwrite8(mf, mx->eshk->shoplevel.dnum);
