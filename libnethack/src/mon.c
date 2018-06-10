5b18edac34d1ac9a8ae1d31bb55e8b9ba61c0751
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-06-11 00:44:46 +0200

    Don't punish players for hitting themselves

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-30 */
+/* Last modified by Fredrik Ljungdahl, 2018-06-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3484,6 +3484,9 @@ punish_elbereth(void)
 void
 setmangry(struct monst *mtmp)
 {
+    if (mtmp == &youmonst)
+        return;
+
     if (idle(mtmp))
         mtmp->mstrategy = st_none;
 
