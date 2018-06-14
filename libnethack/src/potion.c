0964981d16008c8ea96b43d561d32f798a103398
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-06-14 16:34:55 +0200

    Fix grammar: "The hill orc suddenly  asleep!"

--- libnethack/src/potion.c
+++ libnethack/src/potion.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-17 */
+/* Last modified by Fredrik Ljungdahl, 2018-06-14 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -756,7 +756,7 @@ peffects(struct monst *mon, struct obj *otmp, int *nothing, int *unkn)
 
         int dur = rn1(10, 25 - 12 * bcsign(otmp));
         if (you || vis)
-            pline(statusbad, "%s suddenly %s asleep!", Mon, you ? "" : "s");
+            pline(statusbad, "%s suddenly fall%s asleep!", Mon, you ? "" : "s");
         if (!you && sleep_monst(NULL, mon, dur, -1))
             slept_monst(mon);
         break;
