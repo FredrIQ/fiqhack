6b176722f3395c25f0e3f15bc3eaaea61004cb02
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-06-19 18:48:12 +0200

    Fix issues with putting on levi while in lava

--- libnethack/src/trap.c
+++ libnethack/src/trap.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-24 */
+/* Last modified by Fredrik Ljungdahl, 2018-06-19 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2561,11 +2561,9 @@ float_up(struct monst *mon)
         (!you && mon->mtrapped)) {
         struct trap *ttmp;
         ttmp = t_at(level, m_mx(mon), m_my(mon));
-        if (!ttmp) {
-            panic("Levitating out of non-existent trap?");
-            return FALSE;
-        }
-        int ttyp = ttmp->ttyp;
+        int ttyp = 0;
+        if (ttmp)
+            ttyp = ttmp->ttyp;
 
         if (ttyp == PIT || ttyp == SPIKED_PIT) {
             if (you)
