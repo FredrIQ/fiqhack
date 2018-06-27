6892a13b9f254b8dc2c48bbc535d85f2d8870af6
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-06-27 15:37:34 +0200

    Fix "It turns into it"

--- libnethack/src/mon.c
+++ libnethack/src/mon.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-06-11 */
+/* Last modified by Fredrik Ljungdahl, 2018-06-27 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3949,7 +3949,7 @@ newcham(struct monst *mtmp, const struct permonst *mdat,
     if (mtmp->dlevel == level)
         newsym(mtmp->mx, mtmp->my);
 
-    if (msg)
+    if (msg && canclassifymon(mtmp))
         pline(mtmp->mtame ? msgc_petneutral : msgc_monneutral,
               "%s turns into %s!", oldname,
               mdat == &mons[PM_GREEN_SLIME] ? "slime" :
