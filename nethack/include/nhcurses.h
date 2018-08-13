b78660d4ed47d783ef9db4c55c9993c255ced6a2
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-08-13 23:21:25 +0200

    Typo fix

--- nethack/include/nhcurses.h
+++ nethack/include/nhcurses.h
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-26 */
+/* Last modified by Fredrik Ljungdahl, 2018-08-13 */
 /* Copyright (c) Daniel Thaler, 2011                              */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -309,7 +309,7 @@ struct settings {
 
     enum autoable_boolean sidebar;   /* whether to draw the inventory sidebar */
     enum nh_animation animation;     /* when to delay */
-    enum nh_extrawin colorbuc;       /* how to color BUC in inventory */
+    enum nh_colorbuc colorbuc;       /* how to color BUC in inventory */
     enum nh_extrawin extrawin;       /* extra window below game if there's room */
     enum nh_motd_setting show_motd;
     enum nh_menupaging menupaging;
