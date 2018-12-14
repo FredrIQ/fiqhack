3461c38e2b13987f53c6210592d86a6810b79e6f
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-12-14 21:27:51 +0100

    Magic chest object amount is taken as-is, not relying on object memory.

--- libnethack/src/memobj.c
+++ libnethack/src/memobj.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-25 */
+/* Last modified by Fredrik Ljungdahl, 2018-12-14 */
 /* Copyright (c) Fredrik Ljungdahl, 2017. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -490,8 +490,7 @@ remembered_contained(const struct obj *obj)
         chain = gamestate.chest;
 
     for (obj = chain; obj; obj = obj->nobj)
-        if (obj->memory == OM_MEMORY_OK)
-            ret++;
+        ret++;
     return ret;
 }
 
