c8f7ff1760f83159b100d0a220445d55167e5101
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-12-30 20:45:02 +0100

    If a monster who is stuck to you leaves a level, unstick

--- libnethack/src/dog.c
+++ libnethack/src/dog.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-04-30 */
+/* Last modified by Fredrik Ljungdahl, 2018-12-30 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -663,6 +663,16 @@ migrate_to_level(struct monst *mtmp, xchar tolev,       /* destination level */
     d_level new_lev;
     xchar xyflags;
     int num_segs = 0;   /* count of worm segments */
+
+    if (mtmp == u.ustuck) {
+        if (Engulfed)
+            expels(mtmp, mtmp->data, TRUE);
+        else {
+            unstuck(mtmp);
+            if (!sticks(youmonst.data))
+                pline(msgc_statusheal, "You get released!");
+        }
+    }
 
     if (mx_eshk(mtmp))
         set_residency(mtmp, TRUE);
