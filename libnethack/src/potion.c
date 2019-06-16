5ac82c245193e2b18903837e0b6899a5a061a51f
Fredrik Ljungdahl <fredde1994@gmail.com>
2019-06-16 18:24:38 +0200

    Add a missing formal ID case

--- libnethack/src/potion.c
+++ libnethack/src/potion.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2019-01-14 */
+/* Last modified by Fredrik Ljungdahl, 2019-06-16 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1315,8 +1315,10 @@ strange_feeling(struct obj *obj, const char *txt)
         pline(msgc_nospoil,
               "You have a %s feeling for a moment, then it passes.",
               Hallucination ? "normal" : "strange");
-    else
+    else {
         pline(msgc_hint, "%s", txt);
+        tell_discovery(obj);
+    }
 
     if (!obj)   /* e.g., crystal ball finds no traps */
         return;
