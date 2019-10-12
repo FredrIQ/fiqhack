f4d035ee3f084b36d1f26772e342e831a7f0297c
Fredrik Ljungdahl <fredde1994@gmail.com>
2019-10-12 19:14:10 +0200

    Rename Strangle to Breath, since it doesn't just apply to strangulation

--- libnethack/src/botl.c
+++ libnethack/src/botl.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-13 */
+/* Last modified by Fredrik Ljungdahl, 2019-10-12 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -285,7 +285,7 @@ make_player_info(struct nh_player_info *pi)
     if (hallucinating(&youmonst))  /* 8 */
         strncpy(pi->statusitems[pi->nr_items++], "Hallu", ITEMLEN);
     if (strangled(&youmonst))      /* 9 */
-        strncpy(pi->statusitems[pi->nr_items++], "Strangle", ITEMLEN);
+        strncpy(pi->statusitems[pi->nr_items++], "Breath", ITEMLEN);
     if (sliming(&youmonst))        /* 10 */
         strncpy(pi->statusitems[pi->nr_items++], "Slime", ITEMLEN);
     if (petrifying(&youmonst))     /* 11 */
--- nethack/src/status.c
+++ nethack/src/status.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-13 */
+/* Last modified by Fredrik Ljungdahl, 2019-10-12 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -202,7 +202,7 @@ static const struct {
       /* misc fatal */
       { "FoodPois", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
       { "Ill", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
-      { "Strangle", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
+      { "Breath", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
       { "Slime", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
       { "Petrify", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
       { "Zombie!", CLR_BRIGHT_MAGENTA, CLR_BRIGHT_MAGENTA },
