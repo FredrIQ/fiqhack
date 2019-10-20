90a28b90962e105151f4b0959b7685b8a88a7aac
Fredrik Ljungdahl <fredde1994@gmail.com>
2019-10-20 19:32:54 +0200

    Fix typo

--- libnethack/src/mhitm.c
+++ libnethack/src/mhitm.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2019-10-19 */
+/* Last modified by Fredrik Ljungdahl, 2019-10-20 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -249,7 +249,7 @@ fightm(struct monst *mon)
 
         /* Don't use mvismon_at -- we want to allow monsters to successfully
            attack displaced monsters sometimes... */
-        if (x == mtmp->mux && y == mtmp->muy)
+        if (x == mon->mux && y == mon->muy)
             mtmp = &youmonst;
         else
             mtmp = m_at(level, x, y);
