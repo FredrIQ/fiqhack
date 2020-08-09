b26039e8c9be1466adfc4846cdec657e32eff306
Fredrik Ljungdahl <fredde1994@gmail.com>
2020-08-09 03:12:04 +0200

    Minor comment fix

--- libnethack/src/spell.c
+++ libnethack/src/spell.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2020-08-05 */
+/* Last modified by Fredrik Ljungdahl, 2020-08-09 */
 /* Copyright (c) M. Stephenson 1988                               */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -2232,9 +2232,7 @@ dump_spells(void)
 /* Note: only gives sensible results if SPELL_IS_FROM_SPELLBOOK(spell)
    Potentially we could expand this to prayer and the like, but that'd need a
    rewrite of the prayer success code (and mathematical analysis of rnz)
-   TODO: this formula is stupid, look into a potential rework of it
-   TODO: this completely break As spawning with shields of reflection, making
-   them unable to cast sanely due to an unwieldy shield, figure out a fix... */
+   TODO: this formula is stupid, look into a potential rework of it */
 static int
 percent_success(const struct monst *mon, int spell)
 {
