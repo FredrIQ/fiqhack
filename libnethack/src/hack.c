10252f486a50361e986885866831a01ca91d4368
Fredrik Ljungdahl <fredde1994@gmail.com>
2020-08-31 00:31:36 +0200

    Minor fix: shouldn't leak "non-attacker" when hallucinating

--- libnethack/src/hack.c
+++ libnethack/src/hack.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2020-08-30 */
+/* Last modified by Fredrik Ljungdahl, 2020-08-31 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -3189,7 +3189,7 @@ check_interrupt(struct monst *mtmp)
     /* TODO: Should this stop for long worm tails? */
     return (mtmp->m_ap_type != M_AP_FURNITURE && mtmp->m_ap_type != M_AP_OBJECT
             && (!is_hider(mtmp->data) || !mtmp->mundetected) &&
-            (!mtmp->mpeaceful || Hallucination) && !noattacks(mtmp->data) &&
+            (Hallucination || (!mtmp->mpeaceful && !noattacks(mtmp->data))) &&
             !mtmp->msleeping && mtmp->data->mmove && mtmp->mcanmove &&
             !onscary(u.ux, u.uy, mtmp) && canspotmon(mtmp));
 }
--- libnethack/src/monmove.c
+++ libnethack/src/monmove.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2020-08-30 */
+/* Last modified by Fredrik Ljungdahl, 2020-08-31 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -88,7 +88,9 @@ dochugw(struct monst *mtmp)
 
     /* a similar check is in monster_nearby() in hack.c */
     /* check whether hero notices monster and stops current activity */
-    if (!rd && (!mtmp->mpeaceful || Hallucination) &&
+    if (!rd && /* did anything */
+        /* ignore peacefuls or non-attackers (unless hallucinating) */
+        ((!mtmp->mpeaceful && !noattacks(mtmp->data)) || Hallucination) &&
         /* it's close enough to be a threat */
         distu(mtmp->mx, mtmp->my) <= (BOLT_LIM + 1) * (BOLT_LIM + 1) &&
         /* and either couldn't see it before, or it was too far away */
@@ -98,8 +100,7 @@ dochugw(struct monst *mtmp)
 
            TODO: This can spoil the existence of walls in dark areas. */
         newspot && couldsee(mtmp->mx, mtmp->my) &&
-        mtmp->mcanmove && (!noattacks(mtmp->data) || Hallucination) &&
-        !onscary(u.ux, u.uy, mtmp))
+        mtmp->mcanmove && !onscary(u.ux, u.uy, mtmp))
         action_interrupted();
 
     return rd;
