acd0cfc9a533ef5052ba01732ea4bef0391b6b2d
Fredrik Ljungdahl <fredde1994@gmail.com>
2020-09-01 00:49:55 +0200

    Fix sidebar reload being unnecessarily slow

--- nethack/src/sidebar.c
+++ nethack/src/sidebar.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-03 */
+/* Last modified by Fredrik Ljungdahl, 2020-09-01 */
 /* Copyright (c) Daniel Thaler, 2011.                             */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -166,7 +166,6 @@ draw_sidebar(void)
     }
 
     draw_frame();
-    redraw_game_windows();
 }
 
 void
