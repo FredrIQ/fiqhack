0f45b80ee15d2c70ce6e39cf1adce0609b9a78f4
Fredrik Ljungdahl <fredde1994@gmail.com>
2021-01-02 23:33:11 +0100

    Fix a bad check for dungeon topology searching when moving down

--- libnethack/src/dig.c
+++ libnethack/src/dig.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2020-01-08 */
+/* Last modified by Fredrik Ljungdahl, 2021-01-02 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -630,8 +630,6 @@ digactualhole(int x, int y, struct monst *mon, int ttyp)
                     pay_for_damage("ruin", FALSE);
 
             } else {
-                d_level newlevel;
-
                 if (*u.ushops && yours)
                     shopdig(1); /* shk might snatch pack */
                 else if (!flags.mon_moving)
@@ -643,9 +641,8 @@ digactualhole(int x, int y, struct monst *mon, int ttyp)
                       "You fall through...");
                 /* Earlier checks must ensure that the destination level exists
                    and is in the present dungeon. */
-                newlevel.dnum = u.uz.dnum;
-                newlevel.dlevel = u.uz.dlevel + 1;
-                schedule_goto(&newlevel, FALSE, TRUE, FALSE, NULL, NULL);
+                d_level dtmp = get_level_destination(level, 1);
+                schedule_goto(&dtmp, FALSE, TRUE, FALSE, NULL, NULL);
                 /* messages for arriving in special rooms */
                 spoteffects(FALSE);
             }
