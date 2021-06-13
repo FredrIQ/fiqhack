7cd17f590bb685fc7633d86af6d037406a0c77b4
Fredrik Ljungdahl <fredde1994@gmail.com>
2021-06-13 21:45:14 +0200

    Don't use "refrigerate" when referring to magic chests

--- libnethack/src/pickup.c
+++ libnethack/src/pickup.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2020-08-30 */
+/* Last modified by Fredrik Ljungdahl, 2021-06-13 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -1665,7 +1665,8 @@ in_container(struct obj *obj)
 
     if (obj->owornmask & W_WORN) {
         pline_once(msgc_yafm, "You cannot %s something you are wearing.",
-                   Icebox ? "refrigerate" : "stash");
+                   Icebox && !magic_chest(current_container) ?
+                   "refrigerate" : "stash");
         return 0;
     } else if ((obj->otyp == LOADSTONE) && obj->cursed) {
         obj->bknown = 1;
